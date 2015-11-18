eb00b95b9f707762ca4d0f136ec64eec19c25536
bhav31nish <bhav31nish.com>
2015-11-18 17:16:52 +0530

    Starting with redesigned Peaks.cu

--- FF_HEDM/src/Peaks.cu
+++ FF_HEDM/src/Peaks.cu
@@ -6,57 +6,19 @@
 //
 
 #include <stdio.h>
-#include <math.h>
+#include <sys/time.h>
 #include <stdlib.h>
-#include <time.h>
-#include <limits.h>
-#include <sys/stat.h>
 #include <string.h>
-#include <ctype.h>
 #include <stdint.h>
-#include <stdbool.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <stdarg.h>
-#include <fcntl.h>
+#include <unistd.h>
 #include "nldrmd.cuh"
 
 #define deg2rad 0.0174532925199433
 #define rad2deg 57.2957795130823
-#define MAXNHKLS 5000
+#define MAX_LINE_LENGTH 10240
 #define CalcNorm3(x,y,z) sqrt((x)*(x) + (y)*(y) + (z)*(z))
 #define CalcNorm2(x,y) sqrt((x)*(x) + (y)*(y))
 typedef uint16_t pixelvalue;
-
-static inline
-pixelvalue**
-allocMatrixPX(int nrows, int ncols)
-{
-    pixelvalue** arr;
-    int i;
-    arr = (pixelvalue **) malloc(nrows * sizeof(*arr));
-    if (arr == NULL ) {
-        return NULL;
-    }
-    for ( i = 0 ; i < nrows ; i++) {
-        arr[i] = (pixelvalue*) malloc(ncols * sizeof(*arr[i]));
-        if (arr[i] == NULL ) {
-            return NULL;
-        }
-    }
-    return arr;
-}
-
-static inline
-void
-FreeMemMatrixPx(pixelvalue **mat,int nrows)
-{
-    int r;
-    for ( r = 0 ; r < nrows ; r++) {
-        free(mat[r]);
-    }
-    free(mat);
-}
 
 static inline double sind(double x){return sin(deg2rad*x);}
 static inline double cosd(double x){return cos(deg2rad*x);}
@@ -65,269 +27,150 @@ static inline double asind(double x){return rad2deg*(asin(x));}
 static inline double acosd(double x){return rad2deg*(acos(x));}
 static inline double atand(double x){return rad2deg*(atan(x));}
 
-static inline int CheckDirectoryCreation(char Folder[1024])
-{
-	int e;
-    struct stat sb;
-	char totOutDir[1024];
-	sprintf(totOutDir,"%s/",Folder);
-    e = stat(totOutDir,&sb);
-    if (e!=0 && errno == ENOENT){
-		printf("Output directory did not exist, creating %s\n",totOutDir);
-		e = mkdir(totOutDir,S_IRWXU);
-		if (e !=0) {printf("Could not make the directory. Exiting\n");return 0;}
-	}
-	return 1;
-}
-
-static inline void DoImageTransformations(int NrTransOpt, int TransOpt[10], pixelvalue *Image, int NrPixels)
-{
-	int i,j,k,l,m;
-    pixelvalue **ImageTemp1, **ImageTemp2;
-    ImageTemp1 = allocMatrixPX(NrPixels,NrPixels);
-    ImageTemp2 = allocMatrixPX(NrPixels,NrPixels);
-	for (k=0;k<NrPixels;k++) for (l=0;l<NrPixels;l++) ImageTemp1[k][l] = Image[(NrPixels*k)+l];
-	for (k=0;k<NrTransOpt;k++) {
-		if (TransOpt[k] == 1){
-			for (l=0;l<NrPixels;l++) for (m=0;m<NrPixels;m++) ImageTemp2[l][m] = ImageTemp1[l][NrPixels-m-1]; //Inverting Y.
-		} else if (TransOpt[k] == 2){
-			for (l=0;l<NrPixels;l++) for (m=0;m<NrPixels;m++) ImageTemp2[l][m] = ImageTemp1[NrPixels-l-1][m]; //Inverting Z.
-		} else if (TransOpt[k] == 3){
-			for (l=0;l<NrPixels;l++) for (m=0;m<NrPixels;m++) ImageTemp2[l][m] = ImageTemp1[m][l];
-		} else if (TransOpt[k] == 0){
-			for (l=0;l<NrPixels;l++) for (m=0;m<NrPixels;m++) ImageTemp2[l][m] = ImageTemp1[l][m];
-		}
-		for (l=0;l<NrPixels;l++) for (m=0;m<NrPixels;m++) ImageTemp1[l][m] = ImageTemp2[l][m];
-	}
-	for (k=0;k<NrPixels;k++) for (l=0;l<NrPixels;l++) Image[(NrPixels*k)+l] = ImageTemp2[k][l];
-	FreeMemMatrixPx(ImageTemp1,NrPixels);
-	FreeMemMatrixPx(ImageTemp2,NrPixels);
-}
-
-static void
-check (int test, const char * message, ...)
-{
-    if (test) {
-        va_list args;
-        va_start (args, message);
-        vfprintf (stderr, message, args);
-        va_end (args);
-        fprintf (stderr, "\n");
-        exit (EXIT_FAILURE);
-    }
-}
-
-int main(int argc, char *argv[]){ // Arguments: parameter file name, file number, ring number
-	clock_t start, end;
-	if (argc != 3){
-		printf("Not enough arguments, exiting. Use as:\n\t\t\n");
+int main(int argc, char *argv[]){ // Arguments: parameter file name
+	if (argc != 2){
+		printf("Not enough arguments, exiting. Use as:\n\t\t%s %s\n",argv[0],argv[1]);
 		return 1;
 	}
-    double diftotal;
-    start = clock();
-    // Read params file.
+	//Read params file
     char *ParamFN;
     FILE *fileParam;
     ParamFN = argv[1];
-    char aline[1000];
-    fflush(stdout);
+    char line[MAX_LINE_LENGTH];
     fileParam = fopen(ParamFN,"r");
     if (fileParam == NULL){
-		printf("Parameter file could not be read. Exiting\n");
+		printf("Parameter file: %s could not be read. Exiting\n",argv[1]);
 		return 1;
 	}
-    check (fileParam == NULL,"%s file not found: %s", ParamFN, strerror(errno));
-    char *str, dummy[1000], Folder[1024], FileStem[1024], *TmpFolder, darkcurrentfilename[1024], floodfilename[1024], Ext[1024],RawFolder[1024];
-    TmpFolder = "Temp";
-    int LowNr,FileNr;
-    FileNr = atoi(argv[2]);
-    double Thresh, bc=1, Ycen, Zcen, IntSat, OmegaStep, OmegaFirstFile, Lsd, px, Width, Wavelength, LatticeConstant,MaxRingRad;
-    int CellStruct,NrPixels,Padding = 6, StartNr;
-    char fs[1024];
-    int LayerNr;
-    int NrTransOpt=0;
-    int TransOpt[10];
-    int StartFileNr, NrFilesPerSweep;
-    int DoFullImage = 0;
-    int FrameNrOmeChange = 1;
-    double OmegaMissing = 0, MisDir;
-    while (fgets(aline,1000,fileParam)!=NULL){
-		printf("%s\n",aline);
-		fflush(stdout);
-        str = "StartFileNr ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &StartFileNr);
-            continue;
-        }
-        str = "DoFullImage ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &DoFullImage);
-            continue;
-        }
-        str = "NrFilesPerSweep ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &NrFilesPerSweep);
-            continue;
-        }
-        str = "Ext ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %s", dummy, Ext);
-            continue;
-        }
-        str = "RawFolder ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %s", dummy, RawFolder);
-            continue;
-        }
-        str = "Folder ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %s", dummy, Folder);
-            continue;
-        }
-        str = "FileStem ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %s", dummy, fs);
-            continue;
-        }
-        str = "Dark ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %s", dummy, darkcurrentfilename);
-            continue;
-        }
-        str = "Flood ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %s", dummy, floodfilename);
-            continue;
-        }
-        str = "LowerBoundThreshold ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf", dummy, &Thresh);
-            continue;
-        }
-        str = "BeamCurrent ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf", dummy, &bc);
-            continue;
-        }
-        str = "BC ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf %lf", dummy, &Ycen, &Zcen);
-            continue;
-        }
-        str = "UpperBoundThreshold ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf", dummy, &IntSat);
-            continue;
-        }
-        str = "OmegaStep ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf", dummy, &OmegaStep);
-            continue;
-        }
-        str = "OmegaFirstFile ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf", dummy, &OmegaFirstFile);
-            continue;
-        }
-        str = "px ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf", dummy, &px);
-            continue;
-        }
-        str = "Width ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf", dummy, &Width);
-            continue;
-        }
-        str = "LayerNr ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &LayerNr);
-            continue;
-        }
-        str = "CellStruct ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &CellStruct);
-            continue;
-        }
-        str = "NrPixels ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &NrPixels);
-            continue;
-        }
-        str = "Padding ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &Padding);
-            continue;
-        }
-        str = "Wavelength ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf", dummy, &Wavelength);
-            continue;
-        }
-        str = "Lsd ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf", dummy, &Lsd);
-            continue;
-        }
-        str = "StartNr ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &StartNr);
-            continue;
-        }
-        str = "MaxRingRad ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %lf", dummy, &MaxRingRad);
-            continue;
-        }
-        str = "ImTransOpt ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &TransOpt[NrTransOpt]);
-            NrTransOpt++;
-            continue;
-        }
-        str = "FrameOmeChange ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d %lf %lf", dummy, &FrameNrOmeChange, &OmegaMissing, &MisDir);
-            continue;
-        }
+	char *str;
+	int cmpres, StartFileNr, NrFilesPerSweep, NumDarkBegin=0, NumDarkEnd=0,ColBeamCurrent;
+	double OmegaOffset = 0, bc=0;
+	char dummy[MAX_LINE_LENGTH], ParFilePath[MAX_LINE_LENGTH], FileStem[MAX_LINE_LENGTH];
+	while (fgets(line, MAX_LINE_LENGTH, fileParam) != NULL) {
+		str = "ParFilePath ";
+		cmpres = strncmp(line, str, strlen(str));
+		if (cmpres == 0) {
+			sscanf(line, "%s %s", dummy, ParFilePath);
+			continue;
+		}
+		str = "FileStem ";
+		cmpres = strncmp(line, str, strlen(str));
+		if (cmpres == 0) {
+			sscanf(line, "%s %s", dummy, FileStem);
+			continue;
+		}
+		str = "ParFileColBeamCurrent ";
+		cmpres = strncmp(line, str, strlen(str));
+		if (cmpres == 0) {
+			sscanf(line, "%s %d", dummy, &ColBeamCurrent);
+			continue;
+		}
+		str = "StartFileNr ";
+		cmpres = strncmp(line, str, strlen(str));
+		if (cmpres == 0) {
+			sscanf(line, "%s %d", dummy, &StartFileNr);
+			continue;
+		}
+		str = "NrFilesPerSweep ";
+		cmpres = strncmp(line, str, strlen(str));
+		if (cmpres == 0) {
+			sscanf(line, "%s %d", dummy, &NrFilesPerSweep);
+			continue;
+		}
+		str = "NumDarkBegin ";
+		cmpres = strncmp(line, str, strlen(str));
+		if (cmpres == 0) {
+			sscanf(line, "%s %d", dummy, &NumDarkBegin);
+			continue;
+		}
+		str = "NumDarkEnd ";
+		cmpres = strncmp(line, str, strlen(str));
+		if (cmpres == 0) {
+			sscanf(line, "%s %d", dummy, &NumDarkEnd);
+			continue;
+		}
+		str = "OmegaOffset ";
+		cmpres = strncmp(line, str, strlen(str));
+		if (cmpres == 0) {
+			sscanf(line, "%s %lf", dummy, &OmegaOffset);
+			continue;
+		}
+		str = "BeamCurrent ";
+		cmpres = strncmp(line,str,strlen(str));
+		if (cmpres==0){
+			sscanf(line,"%s %lf", dummy, &bc);
+			continue;
+		}
 	}
-	printf("%f\n",Thresh);
-	Width = Width/px;
-	int i,j,k;
-    for (i=0;i<NrTransOpt;i++){
-        if (TransOpt[i] < 0 || TransOpt[i] > 3){printf("TransformationOptions can only be 0, 1, 2 or 3.\nExiting.\n");return 0;}
-        printf("TransformationOptions: %d ",TransOpt[i]);
-        if (TransOpt[i] == 0) printf("No change.\n");
-        else if (TransOpt[i] == 1) printf("Flip Left Right.\n");
-        else if (TransOpt[i] == 2) printf("Flip Top Bottom.\n");
-        else printf("Transpose.\n");
-    }
-
+	FILE *ParFile;
+	ParFile = fopen(ParFilePath,"r");
+	if (ParFile == NULL){
+		printf("ParFile could not be read");
+		return 1;
+	}
+	int i;
+	int NrFramesPerFile[NrFilesPerSweep],CurrFileNrOffset;
+	for (i=0;i<NrFilesPerSweep;i++)
+		NrFramesPerFile[i] = -(NumDarkBegin+NumDarkEnd);
+	char *token, *saveptr,aline[MAX_LINE_LENGTH];
+	int OmegaSign, goodLine, omegafound;
+	double Omegas[NrFilesPerSweep][300],BeamCurrents[NrFilesPerSweep][300],maxBC=0;
+	while (fgets(line, MAX_LINE_LENGTH, ParFile) != NULL) { // we need rotation type, omega value, beamcurrentframe, totalframenumber per file
+		strncpy(aline,line,strlen(line));
+		goodLine = 0;
+		for (str = aline; ; str=NULL){
+			token = strtok_r(str, " ", &saveptr);
+			if (token == NULL) break;
+			if (!strncmp(token,FileStem,strlen(FileStem))){
+				token = strtok_r(str, " ", &saveptr);
+				token = strtok_r(str, " ", &saveptr);
+				CurrFileNrOffset = atoi(token)-StartFileNr;
+				if (CurrFileNrOffset >=0 && CurrFileNrOffset < NrFilesPerSweep){
+					NrFramesPerFile[CurrFileNrOffset]++;
+					goodLine = 1;
+				}
+			}
+		}
+		// Now get the rotation type, omega value, beam current etc
+		if (NrFramesPerFile[CurrFileNrOffset] < -NumDarkBegin + 1) continue;
+		if (goodLine){
+			strncpy(aline,line,strlen(line));
+			omegafound = 0;
+			for (i=1, str = aline; ; i++, str = NULL){
+				token = strtok_r(str, " ", &saveptr);
+				if (token == NULL) break;
+				if (!strncmp(token,"ramsrot",strlen("ramsrot"))){
+					omegafound = 1;
+					OmegaSign = 1;
+				} else if (!strncmp(token,"aero",strlen("aero"))){
+					omegafound = 1;
+					OmegaSign = -1;
+				} else if (!strncmp(token,"preci",strlen("preci"))){
+					omegafound = 1;
+					OmegaSign = 1;
+				}
+				if (omegafound){
+					token  = strtok_r(str," ", &saveptr);
+					token  = strtok_r(str," ", &saveptr);
+					token  = strtok_r(str," ", &saveptr);
+					i+=3;
+					Omegas[CurrFileNrOffset][NrFramesPerFile[CurrFileNrOffset]+NumDarkBegin-1] = atof(token) * OmegaSign + OmegaOffset;
+					omegafound = 0;
+				}
+				if (i == ColBeamCurrent){
+					BeamCurrents[CurrFileNrOffset][NrFramesPerFile[CurrFileNrOffset]+NumDarkBegin-1] = atof(token);
+					maxBC = (maxBC > atof(token)) ? maxBC : atof(token);
+				}
+			}
+		}
+	}
+	for (i=0;i<NrFilesPerSweep;i++){
+		printf("%d \n", NrFramesPerFile[i]);
+		for (int j=0;j<NrFramesPerFile[i];j++){
+			printf("%lf %lf\t",Omegas[i][j],BeamCurrents[i][j]);
+		}
+		printf("\n");
+	}
+	bc = (bc > maxBC) ? bc : maxBC;
+	printf("%lf\n",bc);
 }
