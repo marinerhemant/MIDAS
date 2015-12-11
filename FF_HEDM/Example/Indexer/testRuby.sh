4e2fc048705afb7f3be478da1f2d8a512cd1bb5b
marinerhemant <hemant@sharmas.biz>
2015-12-12 04:35:18 +0530

    Updates

--- FF_HEDM/Example/Indexer/testRuby.sh
+++ FF_HEDM/Example/Indexer/testRuby.sh
@@ -2,9 +2,8 @@ testbin=Ruby/Data.bin
 if [[ ! -f ${testbin} ]]
 then
 	mkdir -p Ruby
-	cd Ruby
-	tar -xvf ../RubyData.tar.gz
+	tar -xvf RubyData.tar.gz
 fi
 
 cd Ruby
-../../../bin/Indexer paramstest.txt 0
+../../../bin/Indexer paramstest.txt $1
