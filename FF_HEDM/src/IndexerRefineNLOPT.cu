c4cbcdfa2c3bf0743c8af8b66d20deb715e3a17d
marinerhemant <hemant@sharmas.biz>
2015-12-16 10:00:29 +0530

    NLOPT

--- FF_HEDM/src/IndexerRefineNLOPT.cu
+++ FF_HEDM/src/IndexerRefineNLOPT.cu
@@ -2610,7 +2610,7 @@ __global__ void FitGrain_NLOPT(RealType *RTParamArr, int *IntParamArr,
 	stop.minf_max = reqmin;
 	nlopt_func f = &pf_posIni;
 	nlopt_result res = NLOPT_SUCCESS;
-	//res = nldrmd_minimize(n,f,trp,xl,xu,x,&minf,xstep,&stop,scratch);
+	res = nldrmd_minimize(n,f,trp,xl,xu,x,&minf,xstep,&stop,scratch);
 	for (i=0;i<n;i++) xout[i] = x[i];
 	if (res !=1) printf("Not optimized completely. %d, %lf\n",res,minf);
 	RealType Pos[3] = {xout[0],xout[1],xout[2]};
