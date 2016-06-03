ba1be89abd6c7442b8464b680d7cd32d53550ed0
Hemant Sharma <hemant@sharmas.biz>
2016-06-03 10:14:10 -0500

    Cleaner sites file generation

--- FF_HEDM/local/setup.sh
+++ FF_HEDM/local/setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 source ${HOME}/.bashrc
-tail ${HOME}/.bashrc
-echo $PATH
 
 LOCAL_DIR=$( pwd )/local
 CHART=/
@@ -14,6 +12,23 @@ echo localhost indexstrains ${LOCAL_DIR}/IndexStrains.sh > ${LOCAL_DIR}/tc.data
 echo localhost peaks ${LOCAL_DIR}/Peaks.sh >> ${LOCAL_DIR}/tc.data
 echo localhost processPeaks ${LOCAL_DIR}/ProcessPeaks.sh >> ${LOCAL_DIR}/tc.data
 echo localhost processPeaksFullImg ${LOCAL_DIR}/ProcessPeaksFullImg.sh >> ${LOCAL_DIR}/tc.data
+
+##### Setup sites file
+sitesStr='   <profile namespace="karajan" key="initialScore">10000</profile>'
+nCPUs=$(cat /proc/cpuinfo | grep processor | wc -l)
+(( nCPUs -- ))
+if [[ ${nCPUs} -lt 10 ]];
+then
+	cpustr="0.0${nCPUs}"
+else
+	cpustr="0.${nCPUs}"
+fi
+
+sitesStr2="   <profile namespace=\"karajan\" key=\"jobThrottle\">${cpustr}</profile>"
+echo $sitesStr > fileTemp.tmp
+echo $sitesStr2 >> fileTemp.tmp
+sed '7r fileTemp.tmp' < ${LOCAL_DIR}/sitesTemplate.xml > ${LOCAL_DIR}/sites.xml
+
 
 ##### Put correct folder paths
 cat ${HOME}/.bashrc | grep -q BINFOLDER
