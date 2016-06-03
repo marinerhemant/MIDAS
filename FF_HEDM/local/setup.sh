dc472741b80676710f66152015324268eb734cef
Hemant Sharma <hemant@sharmas.biz>
2016-06-02 19:30:43 -0500

    Debug

--- FF_HEDM/local/setup.sh
+++ FF_HEDM/local/setup.sh
@@ -9,17 +9,6 @@ CHART=/
 FF_MIDAS_DIR=${LOCAL_DIR%$CHART*}
 BINFOLDER=${FF_MIDAS_DIR}/bin
 
-echo SWIFT is
-which swift
-if [[ $? == 1 ]];
-then
-	echo "SWIFT not found in path"
-	echo "Path is"
-	echo $PATH
-	echo "Exiting."
-	exit
-fi
-
 ##### SETUP tc.data
 echo localhost indexstrains ${LOCAL_DIR}/IndexStrains.sh > ${LOCAL_DIR}/tc.data
 echo localhost peaks ${LOCAL_DIR}/Peaks.sh >> ${LOCAL_DIR}/tc.data
@@ -40,3 +29,5 @@ echo "Congratulations, you can now use FFSingleLayer to run analysis"
 echo "First type:"
 echo "source ~/.bashrc"
 echo "Or restart terminal"
+echo "and check what happens when you type"
+echo "which swift"
