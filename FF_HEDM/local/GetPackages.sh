6a0590979b2547d53fc781bd7985bb27906b931d
Hemant Sharma <hemant@sharmas.biz>
2016-06-08 17:29:44 -0500

    Corrected paths

--- FF_HEDM/local/GetPackages.sh
+++ FF_HEDM/local/GetPackages.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 dirThis=${HOME}/.MIDAS
-rm -rf dirThis
-mkdir -p dirThis
-cd dirThis
+rm -rf $dirThis
+mkdir -p $dirThis
+cd $dirThis
+echo $(pwd)
 wget http://swiftlang.org/packages/swift-0.95-RC6.tar.gz
 wget http://ab-initio.mit.edu/nlopt/nlopt-2.4.2.tar.gz
 tar -xvf swift-0.95-RC6.tar.gz
