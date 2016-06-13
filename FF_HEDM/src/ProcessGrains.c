bb96197ffa769f8abf28c7e6d7ed6aefa10992f4
Hemant Sharma <hemant@sharmas.biz>
2016-06-13 16:04:57 -0500

    Spelling correction

--- FF_HEDM/src/ProcessGrains.c
+++ FF_HEDM/src/ProcessGrains.c
@@ -121,7 +121,7 @@ FindInternalAnglesTwins(int nrIDs, int *IDs, int *IDsPerGrain,
 				AreTwins = fabs(ang - 60) && 
 						 ( fabs(Axis[0]) - fabs(Axis[1]) ) < 0.01 &&
 						 ( fabs(Axis[2]) - fabs(Axis[1]) ) < 0.01;
-				if AreTwins printf("True");
+				if (AreTwins) printf("True");
 				if (fabs(ang) < 0.1 || AreTwins){
 					counter = FindInternalAnglesTwins(nrIDs,IDs,IDsPerGrain,NrIDsPerID,IDsChecked,
 							OPs,ID_IA_Mat,counter,j,ThisID,Radiuses,SGNr);
