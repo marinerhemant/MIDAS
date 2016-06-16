0af0b46dc1901e45a932044170c9ff1013f60c75
Hemant Sharma <hemant@sharmas.biz>
2016-06-16 18:09:48 -0500

    Cleanup of shm copy

--- FF_HEDM/Orthros/SHMOperators.sh
+++ FF_HEDM/Orthros/SHMOperators.sh
@@ -16,12 +16,22 @@ then
   scp Spots.bin Data.bin nData.bin ExtraInfo.bin puppy41:/dev/shm
   scp Spots.bin Data.bin nData.bin ExtraInfo.bin puppy43:/dev/shm
   scp Spots.bin Data.bin nData.bin ExtraInfo.bin puppy44:/dev/shm
-  exit
+elif [[ $1 == 64 ]]
+then
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0100:/dev/shm
+elif [[ $1 == 320 ]]
+then
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0101:/dev/shm
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0102:/dev/shm
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0103:/dev/shm
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0104:/dev/shm
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0105:/dev/shm
+elif [[ $1 == 384 ]]
+then
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0100:/dev/shm
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0101:/dev/shm
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0102:/dev/shm
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0103:/dev/shm
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0104:/dev/shm
+	scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0105:/dev/shm
 fi
-
-scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0100:/dev/shm
-scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0101:/dev/shm
-scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0102:/dev/shm
-scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0103:/dev/shm
-scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0104:/dev/shm
-scp Spots.bin Data.bin nData.bin ExtraInfo.bin pup0105:/dev/shm
