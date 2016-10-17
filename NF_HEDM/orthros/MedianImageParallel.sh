fd1321eae626122d98be1e95fdb7a82abd955145
Hemant Sharma <hemant@sharmas.biz>
2016-10-17 11:13:19 -0500

    Fix LD_LIBRARY_PATH

--- NF_HEDM/orthros/MedianImageParallel.sh
+++ NF_HEDM/orthros/MedianImageParallel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source ${HOME}/.MIDAS/pathsNF
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.MIDAS/LIBTIFF/lib/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HOME}/.MIDAS/LIBTIFF/lib/
 export $LD_LIBRARY_PATH
 
 ${BINFOLDER}/MedianImageLibTiff $1 $2 $3
