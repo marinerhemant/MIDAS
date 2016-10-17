e79aae628d71057e430220c85ab80bf632a56070
Hemant Sharma <hemant@sharmas.biz>
2016-10-17 11:20:49 -0500

    Correct export of path

--- NF_HEDM/orthros/ImageProcessingParallel.sh
+++ NF_HEDM/orthros/ImageProcessingParallel.sh
@@ -2,6 +2,6 @@
 
 source ${HOME}/.MIDAS/pathsNF
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HOME}/.MIDAS/LIBTIFF/lib/
-export $LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
 
 ${BINFOLDER}/ImageProcessingLibTiff $1 $2 $3
--- NF_HEDM/orthros/MedianImageParallel.sh
+++ NF_HEDM/orthros/MedianImageParallel.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 source ${HOME}/.MIDAS/pathsNF
+echo $LD_LIBRARY_PATH
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${HOME}/.MIDAS/LIBTIFF/lib/
-export $LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
 
 ${BINFOLDER}/MedianImageLibTiff $1 $2 $3
