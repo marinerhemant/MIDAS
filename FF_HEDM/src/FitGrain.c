07c3b37a21ed504c0530e7050a49fc506e4bd00e
Hemant Sharma <hemant@sharmas.biz>
2016-11-01 10:54:31 -0500

    FitGrain

--- FF_HEDM/src/FitGrain.c
+++ FF_HEDM/src/FitGrain.c
@@ -830,7 +830,7 @@ int main(int argc, char *argv[])
 	double NonOptP[10] = {p0,p1,p2,RhoD,Lsd,px,Wavelength,Hbeam,Rsample,MinEta};
 	int NonOptPInt[5] = {NrPixels,nOmeRanges,nRings,nSpots,nhkls};
 	double OptP[6] = {tx,ty,tz,yBC,zBC,wedge};
-	double tols[18] = {150,150,150,deg2rad*0.5,deg2rad*0.5,deg2rad*0.5,1,1,1,1,1,1,
+	double tols[18] = {150/100,150/100,150/100,deg2rad*0.5/100,deg2rad*0.5/100,deg2rad*0.5/100,1/100,1/100,1/100,1/100,1/100,1/100,
 		1,1,1,1,1,1}; // 150 microns for position, 0.5 degrees for orient, 1 % for latticeParameter,
 					  // 1 degree for tilts, 1 pixel for BC, 1 degree for wedge
 	
