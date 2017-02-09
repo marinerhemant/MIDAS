5587907ade8677716ca1879a089aee5c81a02230
Hemant Sharma <hemant@sharmas.biz>
2017-02-09 14:44:08 -0600

    cp to local /dev/shm

--- FF_HEDM/Cluster/SHMOperatorsTracking.sh
+++ FF_HEDM/Cluster/SHMOperatorsTracking.sh
@@ -8,6 +8,7 @@ then
 fi
 
 # Trial option, create a bins.tar.gz, copy it over to /dev/shm of head node.
+cp Spots.bin Data.bin nData.bin ExtraInfo.bin /dev/shm
 tar -cvzf bins.tar.gz Spots.bin ExtraInfo.bin
 mkdir -p ${HOME}/swiftwork/bins/
 cp bins.tar.gz ${HOME}/swiftwork/bins
