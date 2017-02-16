480aa39b5ac83031b87810c40f6e3c2c8f0cf046
Hemant Sharma <hemant@sharmas.biz>
2017-02-15 20:36:07 -0600

    Cleaning up for edison

--- FF_HEDM/Cluster/SHMOperatorsTracking.sh
+++ FF_HEDM/Cluster/SHMOperatorsTracking.sh
@@ -22,38 +22,3 @@ then
 	pscp.pssh -v -h ${HOME}/scphosts.txt Spots.bin /dev/shm/Spots.bin
 	pscp.pssh -v -h ${HOME}/scphosts.txt ExtraInfo.bin /dev/shm/ExtraInfo.bin
 fi
-
-#return
-
-#if [[ $1 == 128 ]]
-#then
-  #scp Spots.bin Data.bin nData.bin ExtraInfo.bin /dev/shm
-  #scp Spots.bin ExtraInfo.bin puppy21:/dev/shm
-  #scp Spots.bin ExtraInfo.bin puppy22:/dev/shm
-  #scp Spots.bin ExtraInfo.bin puppy37:/dev/shm
-  #scp Spots.bin ExtraInfo.bin puppy39:/dev/shm
-  #scp Spots.bin ExtraInfo.bin puppy41:/dev/shm
-  #scp Spots.bin ExtraInfo.bin puppy43:/dev/shm
-  #scp Spots.bin ExtraInfo.bin puppy44:/dev/shm
-#elif [[ $1 == 64 ]]
-#then
-	#scp Spots.bin Data.bin nData.bin ExtraInfo.bin /dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0100:/dev/shm
-#elif [[ $1 == 320 ]]
-#then
-	#scp Spots.bin Data.bin nData.bin ExtraInfo.bin /dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0101:/dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0102:/dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0103:/dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0104:/dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0105:/dev/shm
-#elif [[ $1 == 384 ]]
-#then
-	#scp Spots.bin Data.bin nData.bin ExtraInfo.bin /dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0100:/dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0101:/dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0102:/dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0103:/dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0104:/dev/shm
-	#scp Spots.bin ExtraInfo.bin pup0105:/dev/shm
-#fi
