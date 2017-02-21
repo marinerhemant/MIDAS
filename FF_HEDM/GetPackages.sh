ae6047e5549143e2fa596507727edde48687e8b5
Hemant Sharma <hemant@sharmas.biz>
2017-02-21 16:16:50 -0600

    Debug

--- FF_HEDM/GetPackages.sh
+++ FF_HEDM/GetPackages.sh
@@ -5,11 +5,13 @@
 dirThis=${HOME}/.MIDAS
 mkdir -p ${dirThis}
 package=${HOME}/opt/MIDAS/FF_HEDM/Cluster/Packages/${1}.tar.gz
+echo ${package}
 if [ -f ${package} ]
 then
+	echo "Package found for deployment. ${package}"
 	cp ${package} ${dirThis}/
 	cd ${dirThis}
-	tar -xvf ${1}.tar.gz
+	tar -xf ${1}.tar.gz
 fi
 
 if [ ! -d ${dirThis}/NLOPT ]; then # NLOPT INSTALL
