011e214b64836c0c80d01f2581a1f2fd3bcefb7a
Hemant Sharma <hemant@sharmas.biz>
2017-02-22 14:09:59 -0600

    Correct link names

--- NF_HEDM/Cluster/setup.sh
+++ NF_HEDM/Cluster/setup.sh
@@ -12,9 +12,9 @@ echo "BINFOLDER=${BINFOLDER}/" > ${configfile}
 echo "PFDIR=${LOCAL_DIR}/" >> ${configfile}
 echo "SWIFTDIR=${HOME}/.MIDAS/swift-0.95-RC6/bin/" >> ${configfile}
 echo "MACHINE_NAME=${1}" >> ${configfile}
-ln -s ${LOCAL_DIR}/runSingleLayer.sh ${configdir}/MIDAS_V4_NearFieldSingleLayer.sh
-ln -s ${LOCAL_DIR}/runMultipleLayers.sh ${configdir}/MIDAS_V4_NearFieldMultipleLayers.sh
-ln -s ${LOCAL_DIR}/runNFParameters.sh ${configdir}/MIDAS_V4_NearFieldParameters.sh
+ln -s ${LOCAL_DIR}/runSingleLayer.sh ${configdir}/MIDAS_V4_NearFieldSingleLayer
+ln -s ${LOCAL_DIR}/runMultipleLayers.sh ${configdir}/MIDAS_V4_NearFieldMultipleLayers
+ln -s ${LOCAL_DIR}/runNFParameters.sh ${configdir}/MIDAS_V4_NearFieldParameters
 
 echo "Congratulations, you can now use MIDAS to run NeField analysis"
 echo "Go to ${HOME}/.MIDAS folder, there is MIDAS_V4_NearField.....sh files for running analysis"
