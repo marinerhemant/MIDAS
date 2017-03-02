1048ccd686bd539815e6ce23d3f9e5a71ab29342
Hemant Sharma <hemant@sharmas.biz>
2017-03-02 13:18:40 -0600

    Revert ff to individual jobs instead of batch

--- FF_HEDM/Cluster/IndexRefine.swift
+++ FF_HEDM/Cluster/IndexRefine.swift
@@ -5,13 +5,29 @@
 
 type file;
 
-app indexrefine (int spotsinput, string folder)
+app indexrefine (file param, int spotsinput, file hkl, file spotsfile)
 {
-   indexstrains spotsinput folder;
+   indexstrains spotsinput;
 }
 
-string fldr = arg("folder","/data/tomo1/NFTest/");
+file params <"paramstest.txt">;
+file hkl <"hkls.csv">;
+file spotsfile <"SpotsToIndex.csv">;
 
-foreach i in [1:2000] {
-    indexrefine(i, fldr);
+int spots[] = readData("SpotsToIndex.csv");
+
+foreach i in spots {
+    indexrefine(params, i, hkl, spotsfile);
 }
+
+# This is the trial with 2000 jobs
+#type file;
+#app indexrefine (int spotsinput, string folder)
+#{
+#   indexstrains spotsinput folder;
+#}
+#string fldr = arg("folder","/data/tomo1/NFTest/");
+#
+#foreach i in [1:2000] {
+#    indexrefine(i, fldr);
+#}
--- FF_HEDM/Cluster/IndexStrains.sh
+++ FF_HEDM/Cluster/IndexStrains.sh
@@ -5,6 +5,11 @@
 # See LICENSE file.
 #
 source ${HOME}/.MIDAS/paths
-echo "Chunk:"
+echo "Spot ID:"
 echo $1
-python ${PFDIR}/IndexStrains.py $1 $2
+${BINFOLDER}/IndexerLinuxArgsShm paramstest.txt $1
+${BINFOLDER}/FitPosOrStrains paramstest.txt $1
+# This was the idea with 2000 jobs
+#~ echo "Chunk:"
+#~ echo $1
+#~ python ${PFDIR}/IndexStrains.py $1 $2
