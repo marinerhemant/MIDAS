bde250b540eac578c5677c70031a6ea159cc0740
Hemant Sharma <hemant@sharmas.biz>
2017-03-07 16:55:57 -0600

    Debug

--- NF_HEDM/Cluster/mmap.sh
+++ NF_HEDM/Cluster/mmap.sh
@@ -9,8 +9,8 @@ source ${HOME}/.MIDAS/pathsNF
 
 fn=$1
 direct=$( awk '$1 ~ /^DataDirectory/ { print $2 }' ${fn} )
-${BINFOLDER}/MMapImageInfo ${fn}
 pushd ${direct}
+${BINFOLDER}/MMapImageInfo ${fn}
 tar -cvzf binsNF.tar.gz SpotsInfo.bin DiffractionSpots.bin Key.bin OrientMat.bin
 mkdir -p ${HOME}/swiftwork/bins/
 cp binsNF.tar.gz ${HOME}/swiftwork/bins
