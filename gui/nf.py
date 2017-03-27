8d2d0f65825ad4c653bf0529e947da786d7782fe
Hemant Sharma <hemant@sharmas.biz>
2017-03-27 11:44:35 -0500

    Gui updates

--- gui/nf.py
+++ gui/nf.py
@@ -59,7 +59,7 @@ def draw_plot(frameNr,distanceNr): # always the initial framenr and distance, wi
 		imarr2 = stats.threshold(imarr2,threshmin=background)
 	else:
 		imarr2 = imarr
-	a.imshow(imarr2,cmap=plt.get_cmap('bone'),interpolation='nearest',clim=(0,int(vali.get())))
+	a.imshow(imarr2,cmap=plt.get_cmap('bone'),interpolation='nearest',clim=(float(minThreshvar.get()),float(vali.get())))
 	if initplot:
 		initplot = 0
 	else:
@@ -89,6 +89,9 @@ def plot_updater():
 	global fnstem
 	global ndistances
 	global NrPixels
+	global lsd
+	global minThresh
+	newlsd = float(lsdvar.get())
 	newVar = var.get()
 	newframenr = int(r.get())
 	newdist = int(r2.get())
@@ -97,9 +100,20 @@ def plot_updater():
 	newfnstem = fnstemvar.get()
 	newndistances = int(ndistancesvar.get())
 	newNrPixels = int(NrPixelsvar.get())
-	if initplot == 1:
-		draw_plot(framenr,newdist)
-	if (newframenr != framenr) or (newdist != dist) or (newVar != oldVar) or (newvali != initvali) or (newfolder != folder) or (newfnstem != fnstem) or (newndistances != ndistances) or (newNrPixels != NrPixels):
+	newminThresh = float(minThreshvar.get())
+	if ((initplot == 1) or 
+		(newminThresh != minThresh) or
+		(newframenr != framenr) or 
+		(newdist != dist) or 
+		(newVar != oldVar) or 
+		(newvali != initvali) or 
+		(newfolder != folder) or 
+		(newfnstem != fnstem) or 
+		(newndistances != ndistances) or 
+		(newNrPixels != NrPixels) or 
+		(newlsd !=lsd)):
+		minThresh = newminThresh
+		lsd = newlsd
 		NrPixels = newNrPixels
 		ndistances = newndistances
 		fnstem = newfnstem
@@ -109,11 +123,51 @@ def plot_updater():
 		framenr = newframenr
 		dist = newdist
 		draw_plot(framenr,newdist)
+		plotb()
+
+def plotb():
+	global horvert
+	global imarr2
+	global clickpos
+	if horvert == 1:
+		b.clear()
+		xs = [clickpos[0][0],clickpos[1][0]]
+		y = (clickpos[0][1]+clickpos[1][1])/2
+		smallx = int(xs[0])
+		largex = int(xs[1])
+		if xs[0] > xs[1]:
+			smallx = int(xs[1])
+			largex = int(xs[0])
+		xvals = np.linspace(int(smallx),int(largex),num=largex-smallx+1)
+		y = int(y)
+		zs = []
+		for xval in xvals:
+			zs.append(imarr2[y,int(xval)])
+		b.plot(np.array(xvals),zs)
+		b.title.set_text("LineOutHor")
+		canvas.show()
+		canvas.get_tk_widget().grid(row=0,column=0,columnspan=figcolspan,rowspan=figrowspan,sticky=Tk.W+Tk.E+Tk.N+Tk.S)#pack(side=Tk.TOP,fill=Tk.BOTH,expand=1)
+	elif horvert == 2:
+		b.clear()
+		ys = [clickpos[0][1],clickpos[1][1]]
+		x = (clickpos[0][0]+clickpos[1][0])/2
+		smally = int(ys[0])
+		largey = int(ys[1])
+		if ys[0] > ys[1]:
+			smally = int(ys[1])
+			largey = int(ys[0])
+		yvals = np.linspace(int(smally),int(largey),num=largey-smally+1)
+		x = int(x)
+		zs = []
+		for yval in yvals:
+			zs.append(imarr2[int(yval),x])
+		b.plot(np.array(yvals),zs)
+		b.title.set_text("LineOutVert")
+		canvas.show()
+		canvas.get_tk_widget().grid(row=0,column=0,columnspan=figcolspan,rowspan=figrowspan,sticky=Tk.W+Tk.E+Tk.N+Tk.S)#pack(side=Tk.TOP,fill=Tk.BOTH,expand=1)
 
 def onclick(event):
 	global clickpos
-	global horvert
-	global imarr2
 	global lb1
 	global cid
 	clickpos.append((event.xdata, event.ydata))
@@ -121,42 +175,7 @@ def onclick(event):
 		canvas.mpl_disconnect(cid)
 		cid = None
 		lb1.grid_forget()#pack_forget()
-		if horvert == 1:
-			b.clear()
-			xs = [clickpos[0][0],clickpos[1][0]]
-			y = (clickpos[0][1]+clickpos[1][1])/2
-			smallx = int(xs[0])
-			largex = int(xs[1])
-			if xs[0] > xs[1]:
-				smallx = int(xs[1])
-				largex = int(xs[0])
-			xvals = np.linspace(int(smallx),int(largex),num=largex-smallx+1)
-			y = int(y)
-			zs = []
-			for xval in xvals:
-				zs.append(imarr2[y,int(xval)])
-			b.plot(np.array(xvals),zs)
-			b.title.set_text("LineOutHor")
-			canvas.show()
-			canvas.get_tk_widget().grid(row=0,column=0,columnspan=figcolspan,rowspan=figrowspan,sticky=Tk.W+Tk.E+Tk.N+Tk.S)#pack(side=Tk.TOP,fill=Tk.BOTH,expand=1)
-		elif horvert == 2:
-			b.clear()
-			ys = [clickpos[0][1],clickpos[1][1]]
-			x = (clickpos[0][0]+clickpos[1][0])/2
-			smally = int(ys[0])
-			largey = int(ys[1])
-			if ys[0] > ys[1]:
-				smally = int(ys[1])
-				largey = int(ys[0])
-			yvals = np.linspace(int(smally),int(largey),num=largey-smally+1)
-			x = int(x)
-			zs = []
-			for yval in yvals:
-				zs.append(imarr2[int(yval),x])
-			b.plot(np.array(yvals),zs)
-			b.title.set_text("LineOutVert")
-			canvas.show()
-			canvas.get_tk_widget().grid(row=0,column=0,columnspan=figcolspan,rowspan=figrowspan,sticky=Tk.W+Tk.E+Tk.N+Tk.S)#pack(side=Tk.TOP,fill=Tk.BOTH,expand=1)
+		plotb()
 	return
 
 def horline():
@@ -172,22 +191,42 @@ def horline():
 	if button8 is not None:
 		button8.grid_forget()
 		button8 = None
+	if cid is not None:
+		canvas.mpl_disconnect(cid)
+		cid = None
+	if lb1 is not None:
+		lb1.grid_forget()
+		lb1 = None
 	horvert = 1 # 1 for horizontal, 2 for vertical
 	clickpos = []
 	cid = canvas.mpl_connect('button_press_event',onclick)
 	lb1 = Tk.Label(master=root,text="Click two (almost) horizontal points")
-	lb1.grid(row=figrowspan+3,column=3,sticky=Tk.W)#pack(side=Tk.BOTTOM)
+	lb1.grid(row=figrowspan+4,column=4,sticky=Tk.W)#pack(side=Tk.BOTTOM)
 
 def vertline():
 	global cid
 	global clickpos
 	global horvert
 	global lb1
+	global button7
+	global  button8
+	if button7 is not None:
+		button7.grid_forget()
+		button7 = None
+	if button8 is not None:
+		button8.grid_forget()
+		button8 = None
+	if cid is not None:
+		canvas.mpl_disconnect(cid)
+		cid = None
+	if lb1 is not None:
+		lb1.grid_forget()
+		lb1 = None
 	horvert = 2 # 1 for horizontal, 2 for vertical
 	clickpos = []
 	cid = canvas.mpl_connect('button_press_event',onclick)
 	lb1 = Tk.Label(master=root,text="Click two (almost) vertical points")
-	lb1.grid(row=figrowspan+3,column=3,sticky=Tk.W)#pack(side=Tk.BOTTOM)
+	lb1.grid(row=figrowspan+4,column=4,sticky=Tk.W)#pack(side=Tk.BOTTOM)
 
 def top_destroyer():
 	global top2
@@ -216,7 +255,7 @@ def getpos(event):
 	global button7
 	ix,iy = event.xdata,event.ydata
 	button7 = Tk.Button(master=root,text='Confirm Selection',command=confirmselectspot)
-	button7.grid(row=figrowspan+3,column=3,sticky=Tk.W)#pack(side=Tk.LEFT)
+	button7.grid(row=figrowspan+3,column=4,sticky=Tk.E)#pack(side=Tk.LEFT)
 
 def loadnewdistance():
 	global topNewDistance
@@ -248,6 +287,7 @@ def confirmselectspot():
 
 def computedistances():
 	global topDistanceResult
+	global lsd
 	nsols = ndistances*(ndistances-1)/2
 	xs = np.zeros(nsols)
 	ys = np.zeros(nsols)
@@ -258,15 +298,15 @@ def computedistances():
 			z2 = spots[j][1]
 			y1 = spots[i][0]
 			y2 = spots[j][0]
-			#r1 = spots[i][2]
-			#r2 = spots[j][2]
 			x = distDiff * (j-i)
-			xs[idx] = x*z1/(z2-z1) - (distDiff * i)#(r1*x/(r2-r1)) - (distDiff * i)
+			xs[idx] = x*z1/(z2-z1) - (distDiff * i)
 			ys[idx] = y1 + (y2-y1)*z1/(z1-z2)
 			idx += 1
 	topDistanceResult = Tk.Toplevel()
 	topDistanceResult.title("Distance computation result")
 	textdisplay = "Calculated distances are: " + str(xs) + " Calculated Ys are: " + str(ys)
+	lsd = np.mean(xs)
+	lsdvar.set(str(lsd))
 	Tk.Label(topDistanceResult,text=textdisplay,font=("Helvetica",16)).grid(row=0)
 	buttonclose = Tk.Button(master=topDistanceResult,text="Okay",command=topDistanceResult.destroy)
 	buttonclose.grid(row=1)
@@ -295,7 +335,7 @@ def selectspotsfcn():
 	Tk.Label(topSelectSpotsWindow,text="1. Make sure you have the correct Beam centers and difference in distances, otheriwse click Enter Beam Centers.").grid(row=1, column=0)
 	button5 = Tk.Button(master=topSelectSpotsWindow,text='Enter Beam Centers',command=bcwindow)
 	button5.grid(row=1,column=1,sticky=Tk.W)#pack(side=Tk.LEFT)
-	Tk.Label(topSelectSpotsWindow,text="2. Make sure you have median correction enabled.").grid(row=2,columnspan=2)
+	Tk.Label(topSelectSpotsWindow,text="2. It is recommended to have median correction enabled.").grid(row=2,columnspan=2)
 	Tk.Label(topSelectSpotsWindow,text="3. Starting from the first distance, click on or close to a diffraction spot and then click Confirm Selection.").grid(row=3,columnspan=2)
 	Tk.Label(topSelectSpotsWindow,text="4. Repeat this for each distance.").grid(row=4,columnspan=2)
 	Tk.Label(topSelectSpotsWindow,text="5. Click Compute Distances once finished.").grid(row=5,columnspan=2)
@@ -374,7 +414,7 @@ var = Tk.IntVar()
 initplot = 1
 framenr = 20
 dist = 0
-horvert = 1
+horvert = 0
 oldVar = 0
 bcs = np.zeros((ndistances,2))
 spots = np.zeros((ndistances,3))
@@ -399,14 +439,15 @@ topDistanceResult = None
 selectingspots = 0
 clickpos = []
 varsStore = []
+lsd = 0
 canvas.get_tk_widget().grid(row=0,column=0,columnspan=figcolspan,rowspan=figrowspan,sticky=Tk.W+Tk.E+Tk.N+Tk.S)#pack(side=Tk.TOP,fill=Tk.BOTH)
 toolbar_frame = Tk.Frame(root)
-toolbar_frame.grid(row=figrowspan+5,column=0,columnspan=5,sticky=Tk.W)
+toolbar_frame.grid(row=figrowspan+4,column=0,columnspan=5,sticky=Tk.W)
 toolbar = NavigationToolbar2TkAgg( canvas, toolbar_frame )
 toolbar.update()
 
 Tk.Label(master=root,text="Folder").grid(row=figrowspan+1,column=1,sticky=Tk.W)#pack(side=Tk.LEFT)
-foldervar = Tk.StringVar() 
+foldervar = Tk.StringVar()
 foldervar.set(folder)
 e0 = Tk.Entry(master=root,textvariable=foldervar,width=35)
 e0.grid(row=figrowspan+1,column=2,sticky=Tk.W)#pack(side=Tk.LEFT)
@@ -484,33 +525,18 @@ pxvar.set(str(pixelsize))
 epx = Tk.Entry(master=root,textvariable=pxvar,width=5)
 epx.grid(row=figrowspan+2,column=3,sticky=Tk.W,padx=10)#pack(side=Tk.LEFT)
 
-def paramfileselect():
-	global paramfile
-	global parameters
-	paramfile = tkFileDialog.askopenfile()
-	filecontents = paramfile.readlines()
-	print filecontents
+Tk.Label(master=root,text="First Lsd (microns)").grid(row=figrowspan+2,column=3,sticky=Tk.E)#pack(side=Tk.LEFT)
+lsdvar = Tk.StringVar()
+lsdvar.set(str(lsd))
+elsd = Tk.Entry(master=root,textvariable=lsdvar,width=10)
+elsd.grid(row=figrowspan+2,column=4,sticky=Tk.W,padx=10)#pack(side=Tk.LEFT)
 
-def getparams():
-	# open new toplevel window, either read from file or take input, then close
-	global topGetParams
-	global paramfile
-	topGetParams = Tk.Toplevel()
-	topGetParams.title("Load Parameters into memory")
-	Tk.Label(master=topGetParams,text="How do you want to load the parameters? File or input manually?",font=("Helvetica",12)).grid(row=1)
-	bFile = Tk.Button(master=topGetParams,text="Choose File",command=paramfileselect)
-	bFile.grid(row=2)
-	x = 0
-paramfile = ""
-parameters = {}
-buttongetparams = Tk.Button(master=root,text='LoadParameters',command=getparams)
-buttongetparams.grid(row=figrowspan+2,column=3,sticky=Tk.E,padx=10)#pack(side=Tk.LEFT)
-
-def saveparams():
-	# open new toplevel window, either read from file or take input, then close
-	x = 0
-buttonsaveparams = Tk.Button(master=root,text='SaveParameters',command=saveparams)
-buttonsaveparams.grid(row=figrowspan+2,column=4,sticky=Tk.W,padx=10)#pack(side=Tk.LEFT)
+minThresh = 0
+Tk.Label(master=root,text="MinThresh (counts)").grid(row=figrowspan+2,column=4,sticky=Tk.E,padx=100)#pack(side=Tk.LEFT)
+minThreshvar = Tk.StringVar()
+minThreshvar.set(str(minThresh))
+emt = Tk.Entry(master=root,textvariable=minThreshvar,width=10)
+emt.grid(row=figrowspan+2,column=4,sticky=Tk.E,padx=10)#pack(side=Tk.LEFT)
 
 button3 = Tk.Button(master=root,text='LineOutHor',command=horline)
 button3.grid(row=figrowspan+3,column=1,sticky=Tk.W,padx=40)#pack(side=Tk.LEFT)
@@ -523,6 +549,165 @@ button5.grid(row=figrowspan+3,column=2,sticky=Tk.W,padx=10)#pack(side=Tk.LEFT)
 
 button6 = Tk.Button(master=root,text='Select Spots',command=selectspots)
 button6.grid(row=figrowspan+3,column=2,sticky=Tk.E,padx=90)#pack(side=Tk.LEFT)
+
+def killtopGetGrain():
+	global topGetGrain
+	global om,pos,latC,wl,startome,omestep, sg, maxringrad
+	for i in range(9):
+		om[i] = float(omvar[i].get())
+	for i in range(3):
+		pos[i] = float(posvar[i].get())
+	for i in range(6):
+		latC[i] = float(latCvar[i].get())
+	wl = float(wlvar.get())
+	startome = float(startomevar.get())
+	omestep = float(omestepvar.get())
+	sg = int(sgvar.get())
+	maxringrad = float(maxringradvar.get())
+	topGetGrain.destroy()
+
+def getgrain():
+	# open new toplevel window, either read from file or take input, then close
+	global topGetGrain
+	global omvar,posvar,latCvar,wlvar,startomevar,omestepvar,sgvar,maxringradvar
+	topGetGrain = Tk.Toplevel()
+	topGetGrain.title("Load Grain into memory")
+	Tk.Label(master=topGetGrain,text="Please enter the orientation matrix, position, lattice parameter, wavelength, startomega, omegastep, spacegroup, maxringrad",font=("Helvetica",12)).grid(row=1,columnspan=10)
+	Tk.Label(master=topGetGrain,text="Orientation Matrix: ").grid(row=2,column=1)
+	for i in range(9):
+		var1 = Tk.StringVar()
+		var1.set(str(om[i]))
+		Tk.Entry(topGetGrain,textvariable=var1).grid(row=2,column=i+2)
+		omvar.append(var1)
+	Tk.Label(master=topGetGrain,text="Grain Position (microns): ").grid(row=3,column=1)
+	for i in range(3):
+		var1 = Tk.StringVar()
+		var1.set(str(pos[i]))
+		Tk.Entry(topGetGrain,textvariable=var1).grid(row=3,column=i+2)
+		posvar.append(var1)
+	Tk.Label(master=topGetGrain,text="Lattice parameter (Angstrom): ").grid(row=4,column=1)
+	for i in range(6):
+		var1 = Tk.StringVar()
+		var1.set(str(latC[i]))
+		Tk.Entry(topGetGrain,textvariable=var1).grid(row=4,column=i+2)
+		latCvar.append(var1)
+	Tk.Label(master=topGetGrain,text="Wavelength (Angstrom): ").grid(row=5,column=1)
+	wlvar.set(str(wl))
+	Tk.Entry(topGetGrain,textvariable=wlvar).grid(row=5,column=2)
+	Tk.Label(master=topGetGrain,text="StartOmega (Degrees): ").grid(row=6,column=1)
+	startomevar.set(str(startome))
+	Tk.Entry(topGetGrain,textvariable=startomevar).grid(row=6,column=2)
+	Tk.Label(master=topGetGrain,text="OmegaStep (Degrees): ").grid(row=7,column=1)
+	omestepvar.set(str(omestep))
+	Tk.Entry(topGetGrain,textvariable=omestepvar).grid(row=7,column=2)
+	Tk.Label(master=topGetGrain,text="Space Group Number").grid(row=8,column=1)
+	sgvar.set(str(sg))
+	Tk.Entry(topGetGrain,textvariable=sgvar).grid(row=8,column=2)
+	Tk.Label(master=topGetGrain,text="MaxRingRad (microns)").grid(row=9,column=1)
+	maxringradvar.set(str(maxringrad))
+	Tk.Entry(topGetGrain,textvariable=maxringradvar).grid(row=9,column=2)
+	buttonConfirmGrainParams = Tk.Button(master=topGetGrain,text="Confirm",command=killtopGetGrain)
+	buttonConfirmGrainParams.grid(row=10,columnspan=10)
+
+omvar = []
+posvar = []
+latCvar = []
+wlvar = Tk.StringVar()
+startomevar = Tk.StringVar()
+omestepvar = Tk.StringVar()
+sgvar = Tk.StringVar()
+maxringradvar = Tk.StringVar()
+om = np.zeros(9)
+pos = np.zeros(3)
+latC = np.zeros(6)
+wl = 0
+startome = 0
+omestep = 0
+sg = 0
+maxringrad = 0
+buttongetgrain = Tk.Button(master=root,text='LoadGrain',command=getgrain)
+buttongetgrain.grid(row=figrowspan+3,column=2,sticky=Tk.E)#pack(side=Tk.LEFT)
+
+deg2rad = 0.0174532925199433
+rad2deg = 57.2957795130823
+
+def YZ4mREta(R,Eta):
+	return -R*math.sin(Eta*deg2rad),R*math.cos(Eta*deg2rad)
+
+def plot_update_spot():
+	global r, spotnrvar
+	thisome = float(simulatedspots[spotnr-1].split(' ')[2])
+	rad = float(simulatedspots[spotnr-1].split(' ')[0])
+	eta = float(simulatedspots[spotnr-1].split(' ')[1])
+	filenrToRead = int((float(thisome)-float(startome))/omestep)
+	r.set(str(filenrToRead))
+	spotnrvar.set(str(spotnr))
+	# calculate spot position and make a blip on the detector
+	ys,zs = YZ4mREta(rad,eta)
+	ya = pos[0]*math.sin(thisome) + pos[1]*math.cos(thisome)
+	xa = -pos[1]*math.sin(thisome) + pos[0]*math.cos(thisome)
+	yn = NrPixels - (ya + ys*(1-(xa/lsd)))/pixelsize - bcs[dist][0]
+	zn = NrPixels - (zs*(1-(xa/lsd)))/pixelsize - bcs[dist][1]
+	print [ys, ya, yn, zs, zn]
+	plot_updater()
+	a.scatter(yn,zn,s=10,color='red')
+	canvas.show()
+	canvas.get_tk_widget().grid(row=0,column=0,columnspan=figcolspan,rowspan=figrowspan,sticky=Tk.W+Tk.E+Tk.N+Tk.S)#pack(side=Tk.TOP,fill=Tk.BOTH,expand=1)
+
+def incr_spotnr():
+	global spotnr
+	spotnr += 1
+	plot_update_spot()
+
+def decr_spotnr():
+	global spotnr
+	spotnr -= 1
+	plot_update_spot()
+
+def update_spotnr():
+	global spotnr
+	spotnr = int(spotnrvar.get())
+	plot_update_spot()
+
+def makespots():
+	global spotnrvar
+	global simulatedspots
+	global r
+	pfname = "ps.txt"
+	f = open(pfname,'w')
+	f.write("SpaceGroup %d\n"%(sg))
+	f.write("Wavelength %lf\n"%(wl))
+	f.write("Lsd %lf\n"%(lsd))
+	f.write("MaxRingRad %lf\n"%(maxringrad))
+	f.write("LatticeConstant %lf %lf %lf %lf %lf %lf\n"%(latC[0],latC[1],latC[2],latC[3],latC[4],latC[5]))
+	f.close()
+	hklpath = '~/opt/MIDAS/NF_HEDM/bin/GetHKLList '
+	os.system(hklpath+pfname)
+	genseedorpath = '~/opt/MIDAS/NF_HEDM/bin/GenSeedOrientationsFF2NFHEDM '
+	diffrspotspath = '~/opt/MIDAS/NF_HEDM/bin/SimulateDiffractionSpots '
+	orinfn = 'orin.txt'
+	oroutfn = 'orout.txt'
+	instr = "120 %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n"%(om[0],om[1],om[2],om[3],om[4],
+			om[5],om[6],om[7],om[8],pos[0],pos[1],pos[2],latC[0],latC[1],latC[2],latC[3],latC[4],latC[5])
+	f = open(orinfn,'w')
+	f.write(instr)
+	f.close()
+	os.system(genseedorpath+orinfn+' '+oroutfn)
+	os.system(diffrspotspath+str(lsd)+' '+oroutfn)
+	spotsfn = 'SimulatedDiffractionSpots.txt'
+	simulatedspots = open(spotsfn,'r').readlines()
+	Tk.Label(master=root,text="SpotNumber").grid(row=figrowspan+3,column=3,sticky=Tk.E,padx=50)
+	Tk.Entry(root,textvariable=spotnrvar,width=3).grid(row=figrowspan+3,column=3,sticky=Tk.E)
+	plot_update_spot()
+	# put a frame with +,- and load
+	Tk.Button(master=root,text="+",command=incr_spotnr,font=("Helvetica",12)).grid(row=figrowspan+3,column=4,sticky=Tk.W)
+	Tk.Button(master=root,text="-",command=decr_spotnr,font=("Helvetica",12)).grid(row=figrowspan+3,column=4,sticky=Tk.W,padx=50)
+	Tk.Button(master=root,text="Load",command=update_spotnr,font=("Helvetica",12)).grid(row=figrowspan+3,column=4,sticky=Tk.E,padx=50)
+	
+spotnr = 1
+spotnrvar = Tk.StringVar()
+bMakeSpots = Tk.Button(master=root,text="MakeSpots",command=makespots)
+bMakeSpots.grid(row=figrowspan+3,column=3,sticky=Tk.W,padx=10)
 
 button = Tk.Button(master=root,text='Quit',command=_quit,font=("Helvetica",20))
 button.grid(row=figrowspan+1,column=0,rowspan=3,sticky=Tk.W,padx=10)#pack(side=Tk.LEFT)
