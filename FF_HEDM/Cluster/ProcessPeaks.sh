afc100e2dc80c67da52755aa0c42a9d1d78b0525
Hemant Sharma <hemant@sharmas.biz>
2017-03-27 17:06:20 -0500

    Debug

--- FF_HEDM/Cluster/ProcessPeaks.sh
+++ FF_HEDM/Cluster/ProcessPeaks.sh
@@ -5,6 +5,10 @@
 # See LICENSE file.
 #
 source ${HOME}/.MIDAS/paths
+paramfile=$1 # always relative path
+CHART=/
+flr=${paramfile%$CHART*}
+cd $flr
 ${BINFOLDER}/MergeOverlappingPeaks $1 $2
 ${BINFOLDER}/CalcRadius $1 $2
 ${BINFOLDER}/FitTiltBCLsdSample $1
