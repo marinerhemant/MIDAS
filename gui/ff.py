d511198b482a207b0f26f5e0eee8130c20af05d2
Hemant Sharma <hemant@sharmas.biz>
2017-04-07 16:27:55 -0500

    Debug

--- gui/ff.py
+++ gui/ff.py
@@ -586,7 +586,7 @@ def loadbplot():
 		b.set_xlim([lims[0][0],lims[0][1]])
 		b.set_ylim([lims[1][0],lims[1][1]])
 	labels = [item.get_text() for item in b.get_yticklabels()]
-	for labelnr in range(length(labels)):
+	for labelnr in range(len(labels)):
 		labels[labelnr] = NrPixels - labels[labelnr]
 	b.set_yticklabels(labels)
 	numrows, numcols = data.shape
