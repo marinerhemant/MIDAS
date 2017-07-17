5d65aeb1805d6705dfed04db9b6b8c0d6e2073df
Hemant Sharma <hemant@sharmas.biz>
2017-07-17 13:06:10 -0500

    Debug swift ordering of execution

--- FF_HEDM/Cluster/ProcessPeaks.sh
+++ FF_HEDM/Cluster/ProcessPeaks.sh
@@ -15,7 +15,15 @@ echo $( pwd )
 echo "Ring Nr is $2"
 filestem=$( awk '$1 ~ /^FileStem/ { print $2 } ' ${paramfile} )
 layernr=$( awk '$1 ~ /^LayerNr/ { print $2 } ' ${paramfile} )
+startnr=$( awk '$1 ~ /^StartNr/ { print $2 } ' ${paramfile} )
+endnr=$( awk '$1 ~ /^EndNr/ { print $2 } ' ${paramfile} )
 mkdir -p $flr/Ring${2}/PeakSearch/${filestem}_${layernr}
+nfiles=$( ls -l output | wc -l )
+while [[ nfiles != (endnr-startnr + 2) ]];
+do
+	sleep 1
+	nfiles=$( ls -l output | wc -l )
+done
 ${BINFOLDER}/MergeOverlappingPeaks $1 $2
 ${BINFOLDER}/CalcRadius $1 $2
 ${BINFOLDER}/FitTiltBCLsdSample $1
