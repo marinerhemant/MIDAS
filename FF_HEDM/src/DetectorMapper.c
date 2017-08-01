4f5715434d665dd1b50fdcaf9022d200a22aaffc
Hemant Sharma <hemant@sharmas.biz>
2017-08-01 10:30:11 -0500

    Debug

--- FF_HEDM/src/DetectorMapper.c
+++ FF_HEDM/src/DetectorMapper.c
@@ -452,7 +452,7 @@ mapperfcn(
 						RThis = sqrt((YZ[0]+PosMatrix[m][0])*(YZ[0]+PosMatrix[m][0])+(YZ[1]+PosMatrix[m][1])*(YZ[1]+PosMatrix[m][1]));
 						EtaThis = CalcEtaAngle(YZ[0]+PosMatrix[m][0],YZ[1]+PosMatrix[m][1]);
 						if (EtaMin < -180) EtaThis -= 360;
-						if (EtaMin >  180) EtaThis += 360;
+						if (EtaMax >  180) EtaThis += 360;
 						if (RThis   >= RMin   && RThis   <= RMax && 
 							EtaThis >= EtaMin && EtaThis <= EtaMax){
 							Edges[nEdges][0] = YZ[0]+PosMatrix[m][0];
