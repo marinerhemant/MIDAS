d0022cb4d5e06f9c99fc77b14bda50067a8f413f
Hemant Sharma <hemant@sharmas.biz>
2017-08-01 15:30:44 -0500

    Debug

--- FF_HEDM/src/DetectorMapper.c
+++ FF_HEDM/src/DetectorMapper.c
@@ -617,6 +617,9 @@ mapperfcn(
 					//printf("%lf %d %lf %lf %lf %lf %lf %lf\n",Area,nEdges,RMin, RMax, Rt, EtaMin, EtaMax, Eta);
 					if (i== 1000 && j == 517 && EtaMin == 179.5 && k == 0) printf("%d %lf\n",nEdges,Area);
 					if (i== 1000 && j == 517 && EtaMin ==-180.5 && k == 0) printf("%d %lf\n",nEdges,Area);
+					for (m=0;mnEdges;m++){
+						printf("%lf %lf %lf\n",EdgesOut[nEdges][0],EdgesOut[nEdges][1],EtaMin);
+					}
 					if (Area < 1E-5){
 						nrContinued++;
 						continue;
