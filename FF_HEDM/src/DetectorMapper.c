68c353356b47b023227d4e89c061e354e5a6f7a7
Hemant Sharma <hemant@sharmas.biz>
2017-08-01 12:09:25 -0500

    Debug

--- FF_HEDM/src/DetectorMapper.c
+++ FF_HEDM/src/DetectorMapper.c
@@ -592,9 +592,9 @@ mapperfcn(
 						if (BETWEEN(zTempMax,zMin,zMax) == 1){
 							Edges[nEdges][0] = yMax;
 							Edges[nEdges][1] = zTempMax;
+							if (i== 961 && j == 518 && EtaMin == 180.0 && k == 0) printf("EMax YMax %lf %lf %lf %lf %lf %lf %lf \n",Edges[nEdges][0],Edges[nEdges][1],EtaMin,zMin,zMax,zTempMax,yMax);
+							if (i== 961 && j == 518 && EtaMin ==-180.0 && k == 0) printf("EMax YMax %lf %lf %lf %lf %lf %lf %lf \n",Edges[nEdges][0],Edges[nEdges][1],EtaMin,zMin,zMax,zTempMax,yMax);
 							nEdges++;
-							if (i== 961 && j == 518 && EtaMin == 180.0 && k == 0) printf("EMax YMax %lf %lf %lf\n",Edges[nEdges][0],Edges[nEdges][1],EtaMin);
-							if (i== 961 && j == 518 && EtaMin ==-180.0 && k == 0) printf("EMax YMax %lf %lf %lf\n",Edges[nEdges][0],Edges[nEdges][1],EtaMin);
 						}
 						//EtaMin,Max and zMin,Max
 						if (fabs(EtaMin)-90 < 1E-5){
