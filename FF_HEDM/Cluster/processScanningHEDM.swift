96a4ab3ff09107adce06af0bc9eadb1926e82794
Hemant Sharma <hemant@sharmas.biz>
2017-08-15 18:37:03 -0500

    Debug

--- FF_HEDM/Cluster/processScanningHEDM.swift
+++ FF_HEDM/Cluster/processScanningHEDM.swift
@@ -15,7 +15,7 @@ string psfn = arg("ParamsFile","ps.txt");
 string grainsfn = arg("GrainsFile","Grains.csv");
 int nrelements = toInt(arg("nrelements","26569"));
 
-foreach i in [0:nrelements]{
+foreach i in [1:nrelements]{
 	file simx<simple_mapper;location="Output",prefix=strcat("IndexRefine_",i,"_"),suffix=".err">;
 	simx = runIndexRefineScanning(psfn,grainsfn,i,fldr);
 }
