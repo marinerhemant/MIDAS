725e0a4c84143d41c6bdd8a79d2f707a00355a94
Hemant Sharma <hemant@sharmas.biz>
2017-08-29 09:27:10 -0500

    Debug

--- FF_HEDM/Cluster/filterGrainsScanning.py
+++ FF_HEDM/Cluster/filterGrainsScanning.py
@@ -100,8 +100,8 @@ for grainNr in range(nGrains):
 				ringnr = int(line.split()[1])
 		f2 = open(outdir+'/'+'Layer'+str(layernr)+'/IDRings.csv')
 		lines = f2.readlines()
-		origID = lines[sp-startnr].split()[1]
-		if (lines[sp-startnr].split()[0]!= ringnr):
+		origID = lines[sp-startnr-1].split()[1]
+		if (lines[sp-startnr-1].split()[0]!= ringnr):
 			print "IDs did not match. Please check."
 			sys.exit(1)
 		f.write(origID+'\t'+str(sp)+'\t'+str(layernr)+'\t'+str(spotsPositions[sp][0])+'\t'+str(spotsPositions[sp][1])+'\t'+str(spotsPositions[sp][2])+'\n')
