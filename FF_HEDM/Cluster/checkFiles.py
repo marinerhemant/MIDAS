4a630668e3c028b4628980414d216796b2c9cf0a
Hemant Sharma <hemant@sharmas.biz>
2018-02-07 03:01:35 -0600

    Debug

--- FF_HEDM/Cluster/checkFiles.py
+++ FF_HEDM/Cluster/checkFiles.py
@@ -27,7 +27,7 @@ for line in paramcontents:
 		ext = line.split()[1]
 
 if not os.path.isfile(darkfn):
-	print 'FILE ' + darkfn + ' does not exist. Exiting!!!'
+	print 'DARK FILE ' + darkfn + ' does not exist. Exiting!!!'
 	sys.exit(1)
 
 startNr = overallStartNr + nrFilesPerSweep*(layernr-1)
