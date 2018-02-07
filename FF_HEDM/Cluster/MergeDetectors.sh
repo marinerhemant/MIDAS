9ddaf89a8678d5e915521e4b2117ffc0580ee415
Hemant Sharma <hemant@sharmas.biz>
2018-02-07 00:01:22 -0600

    Debug

--- FF_HEDM/Cluster/MergeDetectors.sh
+++ FF_HEDM/Cluster/MergeDetectors.sh
@@ -15,7 +15,7 @@ for (( detNr=1; detNr<=4; detNr++ ))
 do
 	# cp ${foldername}/Detector${detNr}/InputAll.csv ${foldername}/InputAll${detNr}.csv
 	cp ${foldername}/Detector${detNr}/InputAllExtraInfoFittingAll.csv ${foldername}/InputAllExtraInfoFittingAll${detNr}.csv
-	cp ${foldername}/Detector${detNr}/paramstext.txt ${foldername}/paramstest${detNr}.txt
+	cp ${foldername}/Detector${detNr}/paramstest.txt ${foldername}/paramstest${detNr}.txt
 	cp ${foldername}/Detector${detNr}/Layer${layernr}_MultiRing_ps.txt ${foldername}/Layer${layernr}_MultiRing_ps${detNr}.txt
 	cp ${foldername}/Layer${layernr}_MultiRing_ps${detNr}.txt ${paramfile}
 	cp ${foldername}/paramstest${detNr}.txt ${foldername}/paramstest.txt
