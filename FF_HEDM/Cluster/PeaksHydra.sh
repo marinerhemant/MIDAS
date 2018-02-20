268b58fb2c63aa19449c0fd2c01c6d3aec79b39b
Hemant Sharma <hemant@sharmas.biz>
2018-02-20 17:28:21 -0600

    Debug

---
+++ FF_HEDM/Cluster/PeaksHydra.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eu
+
+#
+# Copyright (c) 2014, UChicago Argonne, LLC
+# See LICENSE file.
+#
+
+source ${HOME}/.MIDAS/paths
+FOLDERNAME=$2
+for (( detNr=1; detNr<=4; detNr++ ))
+do
+	python ${PFDIR}/runPeaks.py ${BINFOLDER} $1 ${FOLDERNAME}/Detector${detNr}/ParamFileNames.txt $3
+done
