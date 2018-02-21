39367ffd72c809006a2ec1fe5a9d2e1404d6a4ef
Hemant Sharma <hemant@sharmas.biz>
2018-02-20 20:43:15 -0600

    Debug

--- FF_HEDM/Cluster/PeaksHydra.sh
+++ FF_HEDM/Cluster/PeaksHydra.sh
@@ -9,5 +9,7 @@ source ${HOME}/.MIDAS/paths
 FOLDERNAME=$2
 for (( detNr=1; detNr<=4; detNr++ ))
 do
+	date
 	python ${PFDIR}/runPeaks.py ${BINFOLDER} $1 ${FOLDERNAME}/Detector${detNr}/ParamFileNames.txt $3
 done
+date
