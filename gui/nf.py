fd1badf3d68fa5117b8d09ed1786c329e44a2d4f
Hemant Sharma <hemant@sharmas.biz>
2018-05-18 14:50:10 -0500

    Debug

--- gui/nf.py
+++ gui/nf.py
@@ -980,7 +980,7 @@ logscaler = 0
 # Main funcion
 root = Tk.Tk()
 root.wm_title("NF display v0.1 Dt. 2017/03/25 hsharma@anl.gov")
-figur = Figure(figsize=(20,8.5),dpi=100)
+figur = Figure(figsize=(19,8.3),dpi=100)
 canvas = FigureCanvasTkAgg(figur,master=root)
 a = figur.add_subplot(121,aspect='equal')
 b = figur.add_subplot(122)
