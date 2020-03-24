f7393fb3e9df623a78cdd44fe16523b9c1df88f3
Hemant Sharma <hemant@sharmas.biz>
2020-03-24 17:31:45 -0500

    Debug

--- FF_HEDM/src/ScanningFunctions.c
+++ FF_HEDM/src/ScanningFunctions.c
@@ -1200,6 +1200,7 @@ int main (int argc, char *argv[]){
 	nlopt_destroy(opt);
 
 	// Now calculate strains and write out
+	printf("VoxelNr\tOM0\tOM1\tOM2\tOM3\tOM4\tOM5\tOM6\tOM7\tOM8\tx\ty\tz\tA\tB\tC\talpha\tbeta\tgamma\tE00\tE01\tE02\tE10\tE11\tE12\tE20\tE21\tE22\t\n");
 	FILE *out;
 	out = fopen("refined.csv","w");
 	double LatticeParameterFit[6], StrainTensorSample[3][3];
@@ -1215,15 +1216,11 @@ int main (int argc, char *argv[]){
 		LatticeParameterFit[5] = x[i*9+3+5];
 		Euler2OrientMat(Eul,OM);
 		CalcStrainTensorFableBeaudoin(LatCin, LatticeParameterFit, OM, StrainTensorSample);
-		fprintf(out,"%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf 0.0000 %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",
+		fprintf(out,"%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t0.0000\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n",
 			(double)i,OM[0][0],OM[0][1],OM[0][2],OM[1][0],OM[1][1],OM[1][2],OM[2][0],OM[2][1],OM[2][2],
 			voxelList[i*2+0],voxelList[i*2+1],LatticeParameterFit[0],LatticeParameterFit[1],LatticeParameterFit[2],LatticeParameterFit[3],
-			LatticeParameterFit[4],LatticeParameterFit[5],StrainTensorSample[0][0],StrainTensorSample[0][1],StrainTensorSample[0][2],
-			StrainTensorSample[1][0],StrainTensorSample[1][1],StrainTensorSample[1][2],StrainTensorSample[2][0],StrainTensorSample[2][1],
-			StrainTensorSample[2][2]);
-	}
-	printf("Original Refined\n");
-	for (i=0;i<n;i++) {
-		printf("%lf %lf\n",x_orig[i],x[i]);
+			LatticeParameterFit[4],LatticeParameterFit[5],1e6*StrainTensorSample[0][0],1e6*StrainTensorSample[0][1],1e6*StrainTensorSample[0][2],
+			1e6*StrainTensorSample[1][0],1e6*StrainTensorSample[1][1],1e6*StrainTensorSample[1][2],1e6*StrainTensorSample[2][0],1e6*StrainTensorSample[2][1],
+			1e6*StrainTensorSample[2][2]);
 	}
 }
