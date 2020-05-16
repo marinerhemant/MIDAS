7f6fb9a50f202be0a83a0035d9f868ff3bf0b0d2
Hemant Sharma <hemant@sharmas.biz>
2020-05-15 23:10:47 -0500

    Debug

--- NF_HEDM/src/NFGrainsCalc.c
+++ NF_HEDM/src/NFGrainsCalc.c
@@ -298,7 +298,7 @@ int *GrainNrs;
 
 inline void DFS (int *Pos, int grainNr){
 	long long int Pos1 = getIDX(Pos[0],Pos[1],Pos[2],Dims[1],Dims[2]);
-	if (Euler1[Pos1] != 0) return;
+	if (GrainNrs[Pos1] != 0) return;
 	GrainNrs[Pos1] = grainNr;
 	printf("Grain Nr: %d\n",grainNr);
 	int i;
