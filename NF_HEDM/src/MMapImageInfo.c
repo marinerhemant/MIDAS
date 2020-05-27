3691abe2319270e7317a1b42abe3b6ef28ea9b48
Hemant Sharma <hemant@sharmas.biz>
2020-05-27 14:12:37 -0500

    Debug

--- NF_HEDM/src/MMapImageInfo.c
+++ NF_HEDM/src/MMapImageInfo.c
@@ -145,6 +145,10 @@ ReadBinFiles(
                 BinNr += zthis;
                 printf("%lld %d %d %d %lld %d %d %d %d %d %d\n",BinNr,k,NrOfFiles,NrOfPixels,TempCntr,ythis,zthis,nElements,j,(int)ys[j],(int)zs[j]);
                 fflush(stdout);
+                if (BinNr < 0){
+					printf("Something was wrong with the Binary Files. It contained %d for y and %d for z position. Please check, we are exiting.\n",ythis,zthis);
+					retrun 0;
+				}
                 SetBit(ObsSpotsMat,BinNr);
             }
             fclose(fp);
