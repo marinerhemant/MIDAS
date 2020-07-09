f5c8dbc0f50da70e51e9249d4ba242067f9ff14d
Hemant Sharma <hemant@sharmas.biz>
2020-07-09 10:53:41 -0500

    Debug

--- utils/GE2Tiff.py
+++ utils/GE2Tiff.py
@@ -3,16 +3,16 @@ import os
 import os.path as path
 from PIL import Image
 
-# ~ fName = '/data/tomo1/shastri_mar20_data/shastri_mar20/ge5/ss_ff_line_beam_000019.ge5'
-# ~ dName = '/data/tomo1/shastri_mar20_data/shastri_mar20/ge5/dark_before_000018.ge5'
+fName = '/data/tomo1/shastri_mar20_data/shastri_mar20/ge5/ss_ff_line_beam_000019.ge5'
+dName = '/data/tomo1/shastri_mar20_data/shastri_mar20/ge5/dark_before_000018.ge5'
 # ~ fName = '/data/tomo1/kenesei_preuss_nov18/data/ge5/Au_ff_000022.ge5'
 # ~ dName = '/data/tomo1/kenesei_preuss_nov18/data/ge5/dark_before_000021.ge5'
 # ~ fName = '/data/tomo1/mli_aug18_data/ge3/ss_sam_ff3_000394.ge3'
 # ~ dName = '/data/tomo1/mli_aug18_data/ge3/dark_before_000393.ge3'
-fName = '/data/tomo1/mli_aug18_data/ge3/ss_sam_ff3_000406.ge3'
-dName = '/data/tomo1/mli_aug18_data/ge3/dark_before_000405.ge3'
-# ~ fHead = 8192
-fHead = 8396800
+# ~ fName = '/data/tomo1/mli_aug18_data/ge3/ss_sam_ff3_000406.ge3'
+# ~ dName = '/data/tomo1/mli_aug18_data/ge3/dark_before_000405.ge3'
+fHead = 8192
+# ~ fHead = 8396800
 NrPixels = 2048
 nFrames = 1440
 thresh = 80
