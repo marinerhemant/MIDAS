67ad533476b8ec526adbd13fd07fabcf974a4951
Hemant Sharma <hemant@sharmas.biz>
2020-07-17 10:00:27 -0500

    Debug

--- utils/GE2Tiff.py
+++ utils/GE2Tiff.py
@@ -3,8 +3,10 @@ import os
 import os.path as path
 from PIL import Image
 
-fName = '/data/tomo1/shastri_mar20_data/shastri_mar20/ge5/ss_ff_line_beam_000019.ge5'
-dName = '/data/tomo1/shastri_mar20_data/shastri_mar20/ge5/dark_before_000018.ge5'
+# ~ fName = '/data/tomo1/shastri_mar20_data/shastri_mar20/ge5/ss_ff_line_beam_000019.ge5'
+# ~ dName = '/data/tomo1/shastri_mar20_data/shastri_mar20/ge5/dark_before_000018.ge5'
+fName = '/data/tomo1/AI-HEDM/Training_Dataset/NoTwins_Microstructure_4/FF_Sim_Cycle4_Orig_000001.ge3'
+dName = '/data/tomo1/AI-HEDM/Training_Dataset/NoTwins_Microstructure_4/dark_before_000018.ge5'
 # ~ fName = '/data/tomo1/kenesei_preuss_nov18/data/ge5/Au_ff_000022.ge5'
 # ~ dName = '/data/tomo1/kenesei_preuss_nov18/data/ge5/dark_before_000021.ge5'
 # ~ fName = '/data/tomo1/mli_aug18_data/ge3/ss_sam_ff3_000394.ge3'
@@ -15,7 +17,8 @@ fHead = 8192
 # ~ fHead = 8396800
 NrPixels = 2048
 nFrames = 1440
-thresh = 80
+# ~ thresh = 80
+thresh = 5
 
 dark = np.zeros(NrPixels*NrPixels)
 if path.exists(dName):
