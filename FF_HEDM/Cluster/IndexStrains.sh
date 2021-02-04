8b096cacbbdde777fbb449e72d72ee59191e8632
Hemant Sharma <hemant@sharmas.biz>
2021-02-03 19:47:29 -0600

    Cleanup

--- FF_HEDM/Cluster/IndexStrains.sh
+++ FF_HEDM/Cluster/IndexStrains.sh
@@ -5,14 +5,14 @@
 # See LICENSE file.
 #
 source ${HOME}/.MIDAS/paths
-echo "Spot ID:"
-echo $1
+#echo "Spot ID:"
+#echo $1
 if [[ ${#*} > 1 ]]; then
 	cd $2
 fi
-pwd
-ls -l *.bin
-ls -l /dev/shm/*.bin
+#pwd
+#ls -l *.bin
+#ls -l /dev/shm/*.bin
 ${BINFOLDER}/IndexerLinuxArgsShm paramstest.txt $1
 ${BINFOLDER}/FitPosOrStrains paramstest.txt $1
 # This was the idea with 2000 jobs
