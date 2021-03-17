19f80c39d682d11545bad091d2f0e2cf1646f37e
Hemant Sharma <hemant@sharmas.biz>
2021-03-16 21:02:12 -0500

    Debug

--- NF_HEDM/Cluster/initialsetup.sh
+++ NF_HEDM/Cluster/initialsetup.sh
@@ -56,16 +56,18 @@ then
     mv grid.txt grid_unfilt.txt
     mv gridNew.txt grid.txt
   # Filter HexGrid
-  else if [[ -n $( awk '$1 ~ /^GridMask/ { print }' ${TOP_PARAM_FILE} ) ]];
-  then
-    Xmin=$( awk '$1 ~ /^GridMask/ { print $2 }' ${TOP_PARAM_FILE} )
-    Xmax=$( awk '$1 ~ /^GridMask/ { print $3 }' ${TOP_PARAM_FILE} )
-    Ymin=$( awk '$1 ~ /^GridMask/ { print $4 }' ${TOP_PARAM_FILE} )
-    Ymax=$( awk '$1 ~ /^GridMask/ { print $5 }' ${TOP_PARAM_FILE} )
-    awk ' { if (($3 >= '$Xmin') && ($3 <= '$Xmax') && ($4 >= '$Ymin') && ($4 <= '$Ymax'))  print }' < grid.txt >grid_new.txt
-    mv grid.txt grid_old.txt
-    wc -l <grid_new.txt >grid.txt
-    cat grid_new.txt >>grid.txt
+  else
+    if [[ -n $( awk '$1 ~ /^GridMask/ { print }' ${TOP_PARAM_FILE} ) ]];
+    then
+      Xmin=$( awk '$1 ~ /^GridMask/ { print $2 }' ${TOP_PARAM_FILE} )
+      Xmax=$( awk '$1 ~ /^GridMask/ { print $3 }' ${TOP_PARAM_FILE} )
+      Ymin=$( awk '$1 ~ /^GridMask/ { print $4 }' ${TOP_PARAM_FILE} )
+      Ymax=$( awk '$1 ~ /^GridMask/ { print $5 }' ${TOP_PARAM_FILE} )
+      awk ' { if (($3 >= '$Xmin') && ($3 <= '$Xmax') && ($4 >= '$Ymin') && ($4 <= '$Ymax'))  print }' < grid.txt >grid_new.txt
+      mv grid.txt grid_old.txt
+      wc -l <grid_new.txt >grid.txt
+      cat grid_new.txt >>grid.txt
+    fi
   fi
 fi
 
