465f23227e27b75fedf6b75c70280de9b0346b4b
Hemant Sharma <hemant@sharmas.biz>
2021-04-12 10:54:09 -0500

    Debug

--- NF_HEDM/src/MedianImageLibTiffOMP.c
+++ NF_HEDM/src/MedianImageLibTiffOMP.c
@@ -24,89 +24,89 @@ pixelvalue quick_select(pixelvalue a[], int n) ;
 #define PIX_SWAP(a,b) { pixelvalue temp=(a);(a)=(b);(b)=temp; }
 pixelvalue quick_select(pixelvalue a[], int n)
 {
-    int low, high ;
-    int median;
-    int middle, ll, hh;
-    low = 0 ; high = n-1 ; median = (low + high) / 2;
-    for (;;) {
-        if (high <= low)
-            return a[median] ;
-        if (high == low + 1) {
-            if (a[low] > a[high])
-                PIX_SWAP(a[low], a[high]) ;
-            return a[median] ;
-        }
-        middle = (low + high) / 2;
-        if (a[middle] > a[high])    PIX_SWAP(a[middle], a[high]) ;
-        if (a[low] > a[high])       PIX_SWAP(a[low], a[high]) ;
-        if (a[middle] > a[low])     PIX_SWAP(a[middle], a[low]) ;
-        PIX_SWAP(a[middle], a[low+1]) ;
-        ll = low + 1;
-        hh = high;
-        for (;;) {
-            do ll++; while (a[low] > a[ll]) ;
-            do hh--; while (a[hh]  > a[low]) ;
-            if (hh < ll)
-            break;
-            PIX_SWAP(a[ll], a[hh]) ;
-        }
-        PIX_SWAP(a[low], a[hh]) ;
-        if (hh <= median)
-            low = ll;
-        if (hh >= median)
-            high = hh - 1;
-    }
+	int low, high ;
+	int median;
+	int middle, ll, hh;
+	low = 0 ; high = n-1 ; median = (low + high) / 2;
+	for (;;) {
+		if (high <= low)
+			return a[median] ;
+		if (high == low + 1) {
+			if (a[low] > a[high])
+				PIX_SWAP(a[low], a[high]) ;
+			return a[median] ;
+		}
+		middle = (low + high) / 2;
+		if (a[middle] > a[high])    PIX_SWAP(a[middle], a[high]) ;
+		if (a[low] > a[high])       PIX_SWAP(a[low], a[high]) ;
+		if (a[middle] > a[low])     PIX_SWAP(a[middle], a[low]) ;
+		PIX_SWAP(a[middle], a[low+1]) ;
+		ll = low + 1;
+		hh = high;
+		for (;;) {
+			do ll++; while (a[low] > a[ll]) ;
+			do hh--; while (a[hh]  > a[low]) ;
+			if (hh < ll)
+			break;
+			PIX_SWAP(a[ll], a[hh]) ;
+		}
+		PIX_SWAP(a[low], a[hh]) ;
+		if (hh <= median)
+			low = ll;
+		if (hh >= median)
+			high = hh - 1;
+	}
 }
 #undef PIX_SWAP
 
 pixelvalue**
 allocMatrixInt(int nrows, int ncols)
 {
-    pixelvalue** arr;
-    int i;
-    arr = malloc(nrows * sizeof(*arr));
-    for ( i = 0 ; i < nrows ; i++) {
-        arr[i] = malloc(ncols * sizeof(*arr[i]));
-    }
-    return arr;
+	pixelvalue** arr;
+	int i;
+	arr = malloc(nrows * sizeof(*arr));
+	for ( i = 0 ; i < nrows ; i++) {
+		arr[i] = malloc(ncols * sizeof(*arr[i]));
+	}
+	return arr;
 }
 
 pixelvalue***
 allocMatrix3Int(int nrows, int ncols, int nmats)
 {
-    pixelvalue*** arr;
-    int i,j;
-    arr = malloc(nrows * sizeof(*arr));
-    for ( i = 0 ; i < nrows ; i++) {
-        arr[i] = malloc(ncols * sizeof(*arr[i]));
+	pixelvalue*** arr;
+	int i,j;
+	arr = malloc(nrows * sizeof(*arr));
+	for ( i = 0 ; i < nrows ; i++) {
+		arr[i] = malloc(ncols * sizeof(*arr[i]));
 		for (j=0;j<ncols;j++){
 			arr[i][j] = malloc(nmats * sizeof(*arr[i][j]));
 		}
-    }
-    return arr;
+	}
+	return arr;
 }
 
 void
 FreeMemMatrixInt(pixelvalue **mat,int nrows)
 {
-    int r;
-    for ( r = 0 ; r < nrows ; r++) {
-        free(mat[r]);
-    }
-    free(mat);
+	int r;
+	for ( r = 0 ; r < nrows ; r++) {
+		free(mat[r]);
+	}
+	free(mat);
 }
 
 void
 FreeMemMatrix3Int(pixelvalue ***mat,int nrows, int ncols)
 {
-    int r,c;
-    for ( r = 0 ; r < nrows ; r++) {
+	int r,c;
+	for ( r = 0 ; r < nrows ; r++) {
 		for (c=0;c<ncols;c++){
 			free(mat[r][c]);
 		}
-        free(mat[r]);
-    }
-    free(mat);
+		free(mat[r]);
+	}
+	free(mat);
 }
 
 
@@ -206,88 +206,83 @@ usage(void)
 int
 main(int argc, char *argv[])
 {
-    if (argc != 4)
-    {
-        usage();
-        return 1;
-    }
-
-    clock_t start, end;
-    double diftotal;
-    start = clock();
-
-    // Read params file.
-    char *ParamFN;
-    FILE *fileParam;
-    ParamFN = argv[1];
-    char aline[1000];
+	if (argc != 4)
+	{
+		usage();
+		return 1;
+	}
+	double start_time = omp_get_wtime();
+	// Read params file.
+	char *ParamFN;
+	FILE *fileParam;
+	ParamFN = argv[1];
+	char aline[1000];
 	char fn2[1000],fn[1000], direct[1000], ext[1000], extReduced[1000];
-    fileParam = fopen(ParamFN,"r");
-    char *str, dummy[1000];
-    int LowNr,nLayers,StartNr,NrFilesPerLayer,NrPixels,WFImages=0;
+	fileParam = fopen(ParamFN,"r");
+	char *str, dummy[1000];
+	int LowNr,nLayers,StartNr,NrFilesPerLayer,NrPixels,WFImages=0;
 	nLayers = atoi(argv[2]);
-    while (fgets(aline,1000,fileParam)!=NULL){
-        str = "RawStartNr ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &StartNr);
-            continue;
-        }
+	while (fgets(aline,1000,fileParam)!=NULL){
+		str = "RawStartNr ";
+		LowNr = strncmp(aline,str,strlen(str));
+		if (LowNr==0){
+			sscanf(aline,"%s %d", dummy, &StartNr);
+			continue;
+		}
 		str = "DataDirectory ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %s", dummy, direct);
-            continue;
-        }
-        str = "NrPixels ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &NrPixels);
-            continue;
-        }
-        str = "WFImages ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &WFImages);
-            continue;
-        }
-        str = "NrFilesPerDistance ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %d", dummy, &NrFilesPerLayer);
-            continue;
-        }
-        str = "OrigFileName ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %s", dummy, fn2);
-            continue;
-        }
-        str = "extOrig ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %s", dummy, ext);
-            continue;
-        }
-        str = "extReduced ";
-        LowNr = strncmp(aline,str,strlen(str));
-        if (LowNr==0){
-            sscanf(aline,"%s %s", dummy, extReduced);
-            continue;
-        }
-    }
-    int numProcs = atoi(argv[3]);
+		LowNr = strncmp(aline,str,strlen(str));
+		if (LowNr==0){
+			sscanf(aline,"%s %s", dummy, direct);
+			continue;
+		}
+		str = "NrPixels ";
+		LowNr = strncmp(aline,str,strlen(str));
+		if (LowNr==0){
+			sscanf(aline,"%s %d", dummy, &NrPixels);
+			continue;
+		}
+		str = "WFImages ";
+		LowNr = strncmp(aline,str,strlen(str));
+		if (LowNr==0){
+			sscanf(aline,"%s %d", dummy, &WFImages);
+			continue;
+		}
+		str = "NrFilesPerDistance ";
+		LowNr = strncmp(aline,str,strlen(str));
+		if (LowNr==0){
+			sscanf(aline,"%s %d", dummy, &NrFilesPerLayer);
+			continue;
+		}
+		str = "OrigFileName ";
+		LowNr = strncmp(aline,str,strlen(str));
+		if (LowNr==0){
+			sscanf(aline,"%s %s", dummy, fn2);
+			continue;
+		}
+		str = "extOrig ";
+		LowNr = strncmp(aline,str,strlen(str));
+		if (LowNr==0){
+			sscanf(aline,"%s %s", dummy, ext);
+			continue;
+		}
+		str = "extReduced ";
+		LowNr = strncmp(aline,str,strlen(str));
+		if (LowNr==0){
+			sscanf(aline,"%s %s", dummy, extReduced);
+			continue;
+		}
+	}
+	int numProcs = atoi(argv[3]);
 	StartNr = StartNr + (nLayers-1)*WFImages;
-    sprintf(fn,"%s/%s",direct,fn2);
-    fclose(fileParam);
+	sprintf(fn,"%s/%s",direct,fn2);
+	fclose(fileParam);
 	int ReturnCode;
 	ReturnCode = CalcMedian(fn, nLayers,StartNr,NrPixels,NrFilesPerLayer,ext,extReduced,numProcs);
 	if (ReturnCode == 0){
 		printf("Median Calculation failed. Exiting.");
 		return 0;
 	}
-    end = clock();
-    diftotal = ((double)(end-start))/CLOCKS_PER_SEC;
-    printf("Time elapsed in computing median for layer %d: %f [s]\n",nLayers,diftotal);
-    return 0;
+	double time = omp_get_wtime() - start_time;
+	printf("Finished, time elapsed: %lf seconds.\n",time);
+	return 0;
 }
