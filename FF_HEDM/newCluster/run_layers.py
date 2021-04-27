be7196ab0425ee5fb5bb369a489e10c5803287a4
Hemant Sharma <hemant@sharmas.biz>
2021-04-27 15:24:31 -0500

    Debug

--- FF_HEDM/newCluster/run_layers.py
+++ FF_HEDM/newCluster/run_layers.py
@@ -55,6 +55,7 @@ for layerNr in range(startLayerNr,endLayerNr+1):
 	thisPF.write('Folder '+thisDir+'\n')
 	thisPF.write('LayerNr '+str(layerNr)+'\n')
 	thisPF.write('StartFileNr '+str(thisStartNr)+'\n')
+	thisPF.close()
 	innerFolder = thisDir + 'PeakSearch/'+fStem+' '+str(layerNr)+'/'
 	Path(innerFolder).mkdir(parents=True,exist_ok=True)
 	Path(thisDir+'/Temp').mkdir(parents=True,exist_ok=True)
