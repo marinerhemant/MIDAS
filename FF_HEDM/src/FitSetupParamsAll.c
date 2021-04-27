e8079bac1a5e6a401f30829272324f9d70664f8c
Hemant Sharma <hemant@sharmas.biz>
2021-04-27 15:27:14 -0500

    Debug

--- FF_HEDM/src/FitSetupParamsAll.c
+++ FF_HEDM/src/FitSetupParamsAll.c
@@ -475,7 +475,7 @@ static inline void SortSpots(int nIndices, double **SpotsInfo){
 
 int main(int argc, char *argv[])
 {
-	if (argc != 6) {
+	if (argc != 2) {
 		printf("Usage: %s Params.txt\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
