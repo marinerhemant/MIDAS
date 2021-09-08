a6202e508a41536fcc28452a6294b1c860a67c8f
Hemant Sharma <hemant@sharmas.biz>
2021-09-08 11:22:08 -0500

    Debug

--- DT/runDTrecon.py
+++ DT/runDTrecon.py
@@ -2,7 +2,7 @@ import os
 from os.path import expanduser
 import subprocess
 
-fStem = '/data/tomo1/mpe_mar21_data/mpe_mar21/ge3/TiPt_DAC_s3_dt_PFocus_att000'
+fStem = '/data/tomo1/mpe_mar21_data/mpe_mar21/ge3/TiPt_DAC_s3_dt_PFocus_att000_bot'
 startNr = 2917
 endNr = 2981
 pad = 6
@@ -15,4 +15,5 @@ subprocess.call(expanduser('~/opt/MIDAS/DT/bin/DetectorMapper')+' '+paramFN,shel
 for frameNr in range(startNr,endNr+1):
 	thisFN = f'{fStem}_{str(frameNr).zfill(pad)}{ext}'
 	cmd = f'{expanduser("~/opt/MIDAS/DT/bin/IntegratorPeakFit")} {paramFN} {thisFN} {darkFN}'
+	print(cmd)
 	subprocess.call(cmd,shell=True)
