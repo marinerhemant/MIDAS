025a67869f80677fa3a370817515273a5fb1322e
Hemant Sharma <hemant@sharmas.biz>
2021-09-08 15:46:37 -0500

    Debug

--- DT/runDTrecon.py
+++ DT/runDTrecon.py
@@ -13,7 +13,7 @@ ext = '.ge3'
 darkFN = '/data/tomo1/mpe_mar21_data/mpe_mar21/ge3/dark_before_002916.ge3'
 paramFN = 'ps.txt'
 
-subprocess.call(expanduser('~/opt/MIDAS/DT/bin/DetectorMapper')+' '+paramFN,shell=True)
+# ~ subprocess.call(expanduser('~/opt/MIDAS/DT/bin/DetectorMapper')+' '+paramFN,shell=True)
 
 cmd = f'{expanduser("~/opt/MIDAS/DT/bin/IntegratorPeakFitOMP")} {paramFN} {fStem} {startNr} {endNr} {pad} {ext} {darkFN} {nFrames} {numProcs}'
 subprocess.call(cmd,shell=True)
