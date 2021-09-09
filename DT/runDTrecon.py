aaaf8d2a4899b2439967f29a91ff44de9271be4f
Hemant Sharma <hemant@sharmas.biz>
2021-09-08 20:01:00 -0500

    Debug

--- DT/runDTrecon.py
+++ DT/runDTrecon.py
@@ -11,12 +11,16 @@ numProcs = 32
 ext = '.ge3'
 darkFN = '/data/tomo1/mpe_mar21_data/mpe_mar21/ge3/dark_before_002916.ge3'
 paramFN = 'ps.txt'
-radRange = [500, 600]
+
+radRange = [200, 1200]
 rads = [558, 664, 687]
 rWidth = 10
+rBinSize = 0.25
+
 etaRange = [-220, 220]
 etas = [-180,-90,0,90,180]
 etaWidth = 10
+etaBinSize = 0.3
 
 paramContents = open(paramFN,'r').readlines()
 updF = open(paramFN+'.upd','w')
@@ -24,6 +28,8 @@ for line in paramContents:
 	updF.write(line)
 updF.write(f'RMin {radRange[0]}\n')
 updF.write(f'RMax {radRange[1]}\n')
+updF.write(f'RBinSize {rBinSize}\n')
+updF.write(f'EtaBinSize {etaBinSize}\n')
 updF.write(f'EtaMin {etaRange[0]}\n')
 updF.write(f'EtaMax {etaRange[1]}\n')
 for rad in rads:
