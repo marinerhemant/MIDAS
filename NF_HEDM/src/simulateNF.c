d730f3427812764eea0eba430c55fa548654ceef
Hemant Sharma <hemant@sharmas.biz>
2021-09-21 11:08:15 -0500

    Debug

--- NF_HEDM/src/simulateNF.c
+++ NF_HEDM/src/simulateNF.c
@@ -613,6 +613,7 @@ main(int argc, char *argv[])
 	double OMIn[3][3], FracCalc;
 	FILE *InpMicF;
 	InpMicF = fopen(MicFN,"r");
+	printf("%s\n",MicFN);
 	if (InpMicF == NULL) return 1;
 	char outFN[4096];
 	sprintf(outFN,"%s.output.csv",MicFN);
