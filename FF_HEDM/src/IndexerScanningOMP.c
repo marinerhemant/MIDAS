854847e42eea36fbe0e44488a1cda48599e01620
Hemant Sharma <hemant@sharmas.biz>
2022-02-26 20:05:28 -0600

    Debug

--- FF_HEDM/src/IndexerScanningOMP.c
+++ FF_HEDM/src/IndexerScanningOMP.c
@@ -1177,7 +1177,7 @@ int DoIndexing(int SpotID, int voxNr, double xThis, double yThis, double zThis, 
 		or += orDelta;
 	}
 	fracMatches = (RealType) bestnMatchesIsp/bestnTspotsIsp;
-	printf("%lf\n",fracMatches);
+	printf("%lf %d %d\n",fracMatches,bestnMatchesIsp,bestnTspotsIsp);
 	if (fracMatches > 1 || fracMatches < 0 || (int)bestnTspotsIsp == 0 || (int)bestnMatchesIsp == -1 || bestMatchFound == 0){
 		FreeMemMatrix( GrainMatches, MAX_N_MATCHES);
 		FreeMemMatrix( GrainMatchesT, MAX_N_MATCHES);
