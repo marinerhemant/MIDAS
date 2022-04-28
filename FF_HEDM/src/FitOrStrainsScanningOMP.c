469a410c0a3e48b6a8ab25f4cff5fd85e39be276
Hemant Sharma <hemant@sharmas.biz>
2022-04-28 15:29:46 -0500

    Debug

--- FF_HEDM/src/FitOrStrainsScanningOMP.c
+++ FF_HEDM/src/FitOrStrainsScanningOMP.c
@@ -1581,7 +1581,7 @@ int main(int argc, char *argv[])
 	char *FNs[nSptIDs];
 	SptIDs = malloc(nSptIDs*sizeof(*SptIDs));
 	// Read spotIDs
-	printf("%lf %lf %lf %lf %lf %lf\n",LatCinT[0],LatCinT[1],LatCinT[2],LatCinT[3],LatCinT[4],LatCinT[5]);
+	printf("%d %d %d %lf %lf %lf %lf %lf %lf\n",startRowNr,endRowNr,nSptIDs,LatCinT[0],LatCinT[1],LatCinT[2],LatCinT[3],LatCinT[4],LatCinT[5]);
 	int it;
 	FILE *spotsFile = fopen("SpotsToIndex.csv","r");
 	for (it=0;it<nSptIDs;it++){
