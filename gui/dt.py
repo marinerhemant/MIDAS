ad01eaaf1d2c3623b9544d76e59f7afcbb110f87
shirleyisdreaming <106398688+shirleyisdreaming@users.noreply.github.com>
2023-02-09 09:48:01 -0600

    Update dt.py
    from the reconstruct code, method 1 is double, method 2 is float 32, need to be modified; results from method 1 and method 2 are mirror; method 1 log thing need to be modified;

--- gui/dt.py
+++ gui/dt.py
@@ -1,3 +1,4 @@
+
 import matplotlib
 matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
@@ -19,10 +20,10 @@ def _quit():
     root.destroy()
 
 def getfn_1(fstem):
- return fstem+'/'+samplename+'method1_FileNrs_'+str(StartNr)+'_'+str(EndNr)+'_'+str(Resulttype_m1)+'_Rad_'+str(Rad_m1)+'_pm_'+str(Rwidth)+'_Eta_'+str(Eta_m1)+'_pm_'+str(Ewidth)+'_size_'+str(nframe)+'x'+str(nfile)+'_filter_'+str(filter)+'_float32.bin'
+ return fstem+'/'+samplename+'_method1_FileNrs_'+str(StartNr)+'_'+str(EndNr)+'_'+str(Resulttype_m1)+'_Rad_'+str(Rad_m1)+'_pm_'+str(Rwidth)+'_Eta_'+str(Eta_m1)+'_pm_'+str(etaWidth)+'_size_'+str(nfile)+'x'+str(nframe)+'_filter_'+str(filterNr)+'_'+str(NrPixels_m1)+'x'+str(NrPixels_m1)+'_float32.bin'
 
 def getfn_2(fstem):
- return fstem+'/'+samplename+'method2_FileNrs_'+str(StartNr)+'_'+str(EndNr)+'_'+str(Resulttype_m2)+'_Rad_'+str(Rad_m2)+'_pm_'+str(Rwidth)+'_Eta_'+str(Eta_m2)+'_pm_'+str(Ewidth)+'_size_'+str(nframe)+'x'+str(nfile)+'_float32.bin'
+ return fstem+'/'+samplename+'_FileNrs_'+str(StartNr)+'_'+str(EndNr)+'_'+str(Resulttype_m2)+'_Rad_'+str(Rad_m2)+'_pm_'+str(Rwidth)+'_Eta_'+str(Eta_m2)+'_pm_'+str(etaWidth)+'_size_'+str(nfile)+'x'+str(nframe)+'_recon_'+str(NrPixels_m2)+'x'+str(NrPixels_m2)+'.bin'
 
 def getData(fn,reconSize,Methodnum):
     if Methodnum ==1:
@@ -31,7 +32,10 @@ def getData(fn,reconSize,Methodnum):
      fn=getfn_2(fileStem_m2)
     print("Reading file: " + fn)
     f = open(fn,'rb')
-    data = np.fromfile(f,dtype=np.uint16,count=(reconSize*reconSize))
+    if Methodnum ==1:
+     data = np.fromfile(f,dtype=np.double,count=(reconSize*reconSize))
+    else:
+     data = np.fromfile(f,dtype=np.float32,count=(reconSize*reconSize))
     f.close()
     data = np.reshape(data,(reconSize,reconSize))
     data = data.astype(float)
@@ -39,14 +43,14 @@ def getData(fn,reconSize,Methodnum):
 
 def loadplot_m1():
     global refreshPlot
-    global data_m1
+    global data_m1,fileStem_m1
     threshold = float(thresholdvar_m1.get())
     upperthreshold = float(maxthresholdvar_m1.get())
     data_m1 = getData(fileStem_m1,NrPixels_m1,1)
     refreshPlot = 1
     m1.imshow(data_m1,cmap=plt.get_cmap('bone'),interpolation='nearest',clim=(threshold,upperthreshold))
-    m1.set_xlim([lims[0][0],lims[0][1]])
-    m1.set_ylim([lims[1][0],lims[1][1]])
+    #m1.set_xlim([lims[0][0],lims[0][1]])
+    #m1.set_ylim([lims[1][0],lims[1][1]])
     m1.title.set_text("Method 1")
     canvas.draw()
     canvas.get_tk_widget().grid(row=0,column=0,columnspan=figcolspan,rowspan=figrowspan,sticky=Tk.W+Tk.E+Tk.N+Tk.S)
@@ -57,12 +61,11 @@ def loadplot_m2():
     global NrPixels_m2
     threshold = float(thresholdvar_m2.get())
     upperthreshold = float(maxthresholdvar_m2.get())
-    NrPixels_m2 = int(NrPixelsvar_m2.get())
     data_m2 = getData(fileStem_m2,NrPixels_m2,2)
     refreshPlot = 1
     m2.imshow(data_m2,cmap=plt.get_cmap('bone'),interpolation='nearest',clim=(threshold,upperthreshold))
-    m2.set_xlim([lims[0][0],lims[0][1]])
-    m2.set_ylim([lims[1][0],lims[1][1]])
+    #m2.set_xlim([lims[0][0],lims[0][1]])
+    #m2.set_ylim([lims[1][0],lims[1][1]])
     m2.title.set_text("Method 2")
     canvas.draw()
     canvas.get_tk_widget().grid(row=0,column=0,columnspan=figcolspan,rowspan=figrowspan,sticky=Tk.W+Tk.E+Tk.N+Tk.S)
@@ -70,8 +73,12 @@ def loadplot_m2():
 def readParams():
     global paramFN
     paramFN = paramfilevar.get()
-    global NrPixels_m1,startNr,endNr,samplename,nframe,Rad,Rwidth,Eta,etaWidth,nfile
-    paramContents = open(paramFN,'r').readlines()
+    global NrPixels_m1,StartNr,EndNr,samplename,nframe,Rad,Rwidth,Eta,etaWidth,nfile,filterNr
+    
+    f= open(paramFN,'r')
+    paramContents=f.readlines()
+    Eta=[]
+    Rad=[]
     for line in paramContents:
         if line == '\n':
             continue
@@ -83,27 +90,32 @@ def readParams():
             StartNr = int(line.split()[1])
         if 'endNr' == line.split()[0]:
             EndNr = int(line.split()[1])
-        if 'fstem' == line.split()[0]:
+        if 'fStem' == line.split()[0]:
             samplename = str((line.split()[1]).split('/')[-1])
         if 'nFrames' == line.split()[0]:
             nframe = int(line.split()[1])
         if 'rads'==line.split()[0]:
          lineRadarray=line.split()
          for radN in range(len(lineRadarray)-1):
-          Rad[radN] =lineRadarray[1+radN]
+          Rad.append((int(lineRadarray[1+radN])))
         if 'rWidth'==line.split()[0]:
             Rwidth=int(line.split()[1])
-        if 'eta'==line.split()[0]:
+        if 'etas'==line.split()[0]:
          lineEtaarray=line.split()
          for etaN in range(len(lineEtaarray)-1):
-          Eta[etaN] =lineEtaarray[1+etaN]
+          Eta.append(int(lineEtaarray[1+etaN]))
         if 'etaWidth'==line.split()[0]:
             etaWidth=int(line.split()[1])
-        if 'filter'==line.split()[0]:
+        if 'filt'==line.split()[0]:
             filterNr=int(line.split()[1])
     nfile=EndNr-StartNr+1
     NrPixels_m2=nfile
-            
+    cbox_Rad_1.config(value=Rad)
+    cbox_Eta_1.config(value=Eta)
+    cbox_Rad_2.config(value=Rad)
+    cbox_Eta_2.config(value=Eta)
+    
+    
 def paramfileselect():
     global paramFN
     global paramfilevar
@@ -143,22 +155,28 @@ def replot_m2():
     canvas.get_tk_widget().grid(row=0,column=0,columnspan=figcolspan,rowspan=figrowspan,sticky=Tk.W+Tk.E+Tk.N+Tk.S)
     
 def func1(event):
+    global Resulttype_m1
     Resulttype_m1=cbox_rs_1.get()
 
 def func2(event):
-    Rad_m1=cbox_rs_1.get()
+    global Rad_m1
+    Rad_m1=cbox_Rad_1.get()
 
 def func3(event):
-    Eta_m1=cbox_rs_1.get()
+    global Eta_m1
+    Eta_m1=cbox_Eta_1.get()
 
 def func4(event):
+    global Resulttype_m2
     Resulttype_m2=cbox_rs_2.get()
 
 def func5(event):
-    Rad_m2=cbox_rs_2.get()
+    global Rad_m2
+    Rad_m2=cbox_Rad_2.get()
 
 def func6(event):
-    Eta_m2=cbox_rs_2.get()
+    global Eta_m2
+    Eta_m2=cbox_Eta_2.get()
 
 root = Tk.Tk()
 root.wm_title(" DT Reconstruction display ")
@@ -175,15 +193,15 @@ data_m1 = None
 data_m2 = None
 NrPixels_m1 = 128
 NrPixels_m2 = 55
-Rad= None
-Eta=None
+Rad=[]
+Eta=[]
 paramfilevar = Tk.StringVar()
 paramfilevar.set(paramFN)
 thresholdvar_m1 = Tk.StringVar()
-threshold_m1 = 0
+threshold_m1 = -0.02
 thresholdvar_m1.set(str(threshold_m1))
 maxthresholdvar_m1 = Tk.StringVar()
-maxthresholdvar_m1.set(str(50))
+maxthresholdvar_m1.set(str(1))
 
 thresholdvar_m2 = Tk.StringVar()
 threshold_m2 = 0
@@ -215,7 +233,6 @@ secondRowFrame.grid(row=figrowspan+2,column=1,sticky=Tk.W)
 Tk.Label(master=secondRowFrame,text="Method 1:",font=('Helvetica',15)).grid(row=1,column=1,sticky=Tk.W)
 Tk.Button(master=secondRowFrame,text='OutputFolderM1',command=folderSelector_m1,font=("Helvetica",12)).grid(row=1,column=2,sticky=Tk.W)
 Tk.Label(master=secondRowFrame,text='ResultStyle_m1').grid(row=1,column=3,sticky=Tk.W)
-
 cbox_rs_1 = ttk.Combobox(root)
 cbox_rs_1 = ttk.Combobox(master=secondRowFrame,value=Resultvalue)
 cbox_rs_1.grid(row=1,column=4,sticky=Tk.W)
