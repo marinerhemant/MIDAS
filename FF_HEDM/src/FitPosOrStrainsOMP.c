1c5400f1e5b63d4a63d8aecd5198294675752dd2
Hemant Sharma <hemant@sharmas.biz>
2024-03-04 22:03:27 -0600

    Debug

--- FF_HEDM/src/FitPosOrStrainsOMP.c
+++ FF_HEDM/src/FitPosOrStrainsOMP.c
@@ -1585,7 +1585,7 @@ int main(int argc, char *argv[])
 		IA0 = locArr[0];
 		for (i=0;i<9;i++) Orient0[i] = locArr[i+1];
 		for (i=0;i<3;i++) Pos0[i] = locArr[i+10];
-		printf("%d %d %lf %lf %lf\n",thisRowNr,rowNr,Pos0[0],Pos0[1],Pos0[2]);
+		printf("%d %d %d %lf %lf %lf\n",thisRowNr,rowNr,(int)offst1,Pos0[0],Pos0[1],Pos0[2]);
 		NrExpected = locArr[13];
 		NrObserved = locArr[14];
 		free(locArr);
