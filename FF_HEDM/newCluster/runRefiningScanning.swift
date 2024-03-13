21c222ebfe1dda7819adc700243c8254f14da872
Hemant Sharma <hemant@sharmas.biz>
2024-03-12 20:53:13 -0500

    Modified to take the number of jobs as an argument

--- FF_HEDM/newCluster/runRefiningScanning.swift
+++ FF_HEDM/newCluster/runRefiningScanning.swift
@@ -5,20 +5,20 @@
 
 type file;
 
-app (file out) runIndexingScanning (string folder, int blockNr, int numBlocks, int numScans, int numProcs){
-	refineScanning folder blockNr numBlocks numScans numProcs stdout=filename(out);
+app (file out) runIndexingScanning (string folder, int blockNr, int numBlocks, int nIDs, int numProcs){
+	refineScanning folder blockNr numBlocks nIDs numProcs stdout=filename(out);
 }
 
 # Parameters to be supplied ###
 string folder = arg("folder","");
 int nrNodes = toInt(arg("nrNodes","11"));
-int nScans = toInt(arg("nScans","117"));
+int nIDs = toInt(arg("nIDs","117"));
 int numProcs = toInt(arg("numProcs","32"));
 # End Parameters ##############
 
 file indexings[];
 foreach nodeNr in [0:nrNodes-1] {
 	file indexing<simple_mapper;location=strcat(folder,"/output"),prefix=strcat("Indexing_",nodeNr,"_"),suffix=".out">;
-	indexing = runIndexingScanning(folder,nodeNr,nrNodes,nScans,numProcs);
+	indexing = runIndexingScanning(folder,nodeNr,nrNodes,nIDs,numProcs);
 	indexings[nodeNr] = indexing;
 }
