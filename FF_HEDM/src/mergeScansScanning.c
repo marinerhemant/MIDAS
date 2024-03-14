f1f0127db0aadfc76714f62767386f9f1f0b7949
Hemant Sharma <hemant@sharmas.biz>
2024-03-13 23:58:30 -0500

    Debug

--- FF_HEDM/src/mergeScansScanning.c
+++ FF_HEDM/src/mergeScansScanning.c
@@ -102,9 +102,8 @@ main(int argc, char *argv[])
                                     newWeight = thisSpots[i*14+3];
                                     for (k=0;k<14;k++) {
                                         allSpots[j*14+k] = (allSpots[j*14+k]*origWeight + thisSpots[i*14+k]*newWeight)/(origWeight+newWeight);
-                                        printf("%lf ",allSpots[j*14+k]);
                                     }
-                                    printf("\n");
+                                    printf("%lf %lf \n",origWeight,newWeight);
                                 }
                             }
                         }
