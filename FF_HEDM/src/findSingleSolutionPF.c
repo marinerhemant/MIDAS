e32fd954927699dec42c30c8b1a5e928aae010f2
Hemant Sharma <hemant@sharmas.biz>
2024-03-14 01:51:10 -0500

    Debug

--- FF_HEDM/src/findSingleSolutionPF.c
+++ FF_HEDM/src/findSingleSolutionPF.c
@@ -351,6 +351,7 @@ main(int argc, char *argv[])
                                 locThis = ((size_t)allSpotsFin[k*5+3])*maxNHKLs*nScans;
                                 locThis += ((size_t)allSpotsFin[k*5+4])*nScans;
                                 locThis += i;
+                                printf("%lf\n",ArrThis[j*5+1]);
                                 sinoArr[locThis] = ArrThis[j*5+1];
                                 locThis = ((size_t)allSpotsFin[k*5+3])*maxNHKLs + ((size_t)allSpotsFin[k*5+4]);
                                 if (omeArr[locThis]==0) {
