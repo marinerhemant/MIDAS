2e583d1bb0d47cb98ca895e65d41fcbfc6b6fb3d
Hemant Sharma <hemant@sharmas.biz>
2024-03-14 01:01:43 -0500

    Debug

--- FF_HEDM/src/IndexerScanningOMP.c
+++ FF_HEDM/src/IndexerScanningOMP.c
@@ -1313,6 +1313,7 @@ int DoIndexing(int SpotID, int voxNr, double xThis, double yThis, double zThis, 
 	int *outArr2;
 	outArr2 = malloc(rownr*sizeof(*outArr2));
 	for (i=0;i<rownr;i++) outArr2[i] = (int)AllGrainSpots[i][14];
+	printf("%d ",rownr);
 	for (i=0;i<rownr;i++) printf("%d ",outArr2[i]);
 	printf("\n");
 	fwrite(outArr2,rownr*sizeof(int),1,allF);
