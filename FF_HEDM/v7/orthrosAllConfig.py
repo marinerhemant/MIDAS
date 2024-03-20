91429aa129cfef65b2cf30fe11549ac114042fcd
Hemant Sharma <hemant@sharmas.biz>
2024-03-20 00:31:43 -0500

    Debug

--- FF_HEDM/v7/orthrosAllConfig.py
+++ FF_HEDM/v7/orthrosAllConfig.py
@@ -15,7 +15,7 @@ orthrosAllConfig = Config(
                 max_blocks=11,
                 # walltime="150:00:00",
                 scheduler_options='''#$ -q sec1new7.q
-#$ -pe sec1new7 11
+#$ -pe sec1new7 1
 #$ -l h_rt=175:59:00
 #$ -l s_rt=175:58:50''',     # Input your scheduler_options if needed
                 worker_init='source /clhome/TOMO1/opt/midasconda3/bin/activate',     # Input your worker_init if needed
