b75fb1c13b21d6ba48662fd3da55c59436faeedd
Hemant Sharma <hemant@sharmas.biz>
2024-03-21 14:36:41 -0500

    Debug

--- utils/processFilesParallel.py
+++ utils/processFilesParallel.py
@@ -14,7 +14,7 @@ def runSingle(workData):
     lN = workData[2]
     nc = workData[3]
     cwd = os.getcwd()
-    binloc = os.path.expanduser('~/opt/MIDAS/utils/ffGenerate.py')
+    binloc = os.path.expanduser('~/opt/MIDAS/utils/ffGenerateZip.py')
     cmd = f'/nfs/turbo/meche-abucsek/Wenxi/ESRF_Ti_v7/.venv/bin/python {binloc} -resultFolder {rf} -paramFN {pf} -LayerNr {lN} -numFrameChunks {nc}'
     print(cmd)
     subprocess.call(cmd,cwd=cwd,shell=True)
