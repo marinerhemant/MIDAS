a55e38f502a3d992fe12cf03462c25321d2f75b4
Hemant Sharma <hemant@sharmas.biz>
2024-03-21 21:45:27 -0500

    Debug

--- FF_HEDM/src/MergeOverlappingPeaksAllZarr.c
+++ FF_HEDM/src/MergeOverlappingPeaksAllZarr.c
@@ -254,6 +254,7 @@ int main(int argc, char *argv[]){
             dsize = 4096;
             Folder = (char*)malloc((size_t)dsize);
             dsize = blosc1_decompress(arr,Folder,dsize);
+			printf("%d %s\n",Folder);
             free(arr);
             free(data);
         }
