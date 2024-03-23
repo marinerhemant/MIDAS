ee0f515b176cf3c3c1293cd6abe9f654fd4439a1
Hemant Sharma <hemant@sharmas.biz>
2024-03-23 14:34:53 -0500

    Debug

--- FF_HEDM/src/IndexerScanningOMP.c
+++ FF_HEDM/src/IndexerScanningOMP.c
@@ -1330,7 +1330,7 @@ int DoIndexing(int SpotID, int voxNr, double xThis, double yThis, double zThis, 
 	int matchedNrSpots = GrainMatches[0][13];
 	outArr2 = malloc(matchedNrSpots*sizeof(*outArr2));
 	for (i=0;i<matchedNrSpots;i++) outArr2[i] = (int)AllGrainSpots[i][14];
-	for (i=0;i<matchedNrSpots;i++) printf("%d\n",outArr2[i]);
+	for (i=0;i<matchedNrSpots;i++) printf("%d %lf %lf\n",outArr2[i],AllGrainSpots[i][14],AllGrainSpots[i][2],AllGrainSpots[i][5]);
 	fwrite(outArr2,matchedNrSpots*sizeof(int),1,allF);
 	fprintf(keyF,"%zu %zu %zu %zu\n",(size_t)SpotID,(size_t)matchedNrSpots,locVals,locAll);
 	printf("ID: %d, voxNr: %d, Confidence: %lf\n",SpotID,voxNr,fracMatches);
