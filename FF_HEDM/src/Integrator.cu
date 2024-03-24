aa789fd678bea041052e7e5ada0f95c69a78f271
Hemant Sharma <hemant@sharmas.biz>
2024-03-24 00:09:56 -0500

    Debug

--- FF_HEDM/src/Integrator.cu
+++ FF_HEDM/src/Integrator.cu
@@ -806,6 +806,7 @@ int main(int argc, char **argv)
 	clock_t t1, t2,t3,t4,t5,t6;
 	double diffT=0, diffT2=0,diffT3=0;
 	for (i=0;i<nFrames;i++){
+		for (i=0;j<bigArrSize;j++) PerFrameArr[j] = -1;
 		if (chunkFiles>0){
 			if ((i%chunkFiles) == 0){
 				memset(chunkArr,0,bigArrSize*sizeof(*chunkArr));
