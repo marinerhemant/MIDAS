cdd8d7d7207eddc50b338c90a235898a3bd68cd5
Hemant Sharma <hemant@sharmas.biz>
2024-03-25 19:39:33 -0500

    Debug

--- FF_HEDM/src/PeaksFittingOMPZarr.c
+++ FF_HEDM/src/PeaksFittingOMPZarr.c
@@ -1369,7 +1369,7 @@ void main(int argc, char *argv[]){
 		size_t szIn,szOut,blkSz;
 		// blosc1_cbuffer_sizes(&allData[sizeArr[FileNr*2+1]],szIn,szOut,blkSz);
 		// printf("%zu %zu %zu\n",szIn,szOut,blkSz);
-		blosc1_cbuffer_sizes(compData,szIn,szOut,blkSz);
+		blosc1_cbuffer_sizes(compData,&szIn,&szOut,&blkSz);
 		printf("%zu %zu %zu\n",szIn,szOut,blkSz);
 		dsz = blosc1_decompress(compData,locData,dsz);
 		// free(compData);
