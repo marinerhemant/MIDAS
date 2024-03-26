677e77a7bac31a78d80a7b978a7f4a14276a2fc0
Hemant Sharma <hemant@sharmas.biz>
2024-03-25 19:47:34 -0500

    Debug

--- FF_HEDM/src/PeaksFittingOMPZarr.c
+++ FF_HEDM/src/PeaksFittingOMPZarr.c
@@ -1364,9 +1364,9 @@ void main(int argc, char *argv[]){
 		locData = &locDataAll[asym_idxoffset];
 		double t1 = omp_get_wtime();
 		size_t szThsArr = sizeArr[FileNr*2+0];
-		dsz = blosc1_decompress(&allData[sizeArr[FileNr*2+1]],locData,dsz);
+		// dsz = blosc1_decompress(&allData[sizeArr[FileNr*2+1]],locData,dsz);
 		double t2 = omp_get_wtime();
-        // memcpy(ImageAsym,locData,dsz);
+        memcpy(ImageAsym,locData,dsz);
 		MakeSquare(NrPixels,NrPixelsY,NrPixelsZ,ImageAsym,Image);
 		if (makeMap == 1){
 			int badPxCounter = 0;
