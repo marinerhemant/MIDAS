79535453eefee3057d080f93551fa491e86102be
Hemant Sharma <hemant@sharmas.biz>
2024-04-25 14:28:57 -0500

    Debug

--- utils/integrator.py
+++ utils/integrator.py
@@ -8,6 +8,7 @@ import shutil
 import re
 import zarr
 import fsspec
+from multiprocessing import Pool
 utilsDir = os.path.expanduser('~/opt/MIDAS/utils/')
 sys.path.insert(0,utilsDir)
 from midas2zip import Hdf5ToZarr
@@ -37,12 +38,37 @@ def generateZip(resFol,pfn,dfn='',dloc='',nchunks=-1,preproc=-1,outf='ZipOut.txt
         cmd+= ' -numFrameChunks '+str(nchunks)
     if preproc!=-1:
         cmd+= ' -preProcThresh '+str(preproc)
-    outf = f'{resFol}/{logdir}/{outf}'
-    errf = f'{resFol}/{logdir}/{errf}'
+    outf = f'{resFol}/{logdir}/{dfn}_{outf}'
+    errf = f'{resFol}/{logdir}/{dfn}_{errf}'
     subprocess.call(cmd,shell=True,stdout=open(outf,'w'),stderr=open(errf,'w'))
     lines = open(outf,'r').readlines()
     if lines[-1].startswith('OutputZipName'):
         return lines[-1].split()[1]
+
+def runOneFile(fileNr):
+    thisFN = InputFN.replace(startFileNrStr,str(startFileNr+fileNr).zfill(6))
+    if convertFiles == 1:
+        zipFN = generateZip(resultDir,psFN,dfn=thisFN,nchunks=numFrameChunks,preproc=preProc)
+    else:
+        if thisFN[-3:] != 'zip':
+            thisFN += '.analysis.MIDAS.zip'
+        zipFN = resultDir + thisFN
+        print(f'Processing file: {zipFN}')
+    f = open(f'{resultDir}/{logdir}/{os.path.basename(zipFN)}_integrator_out.csv','w')
+    f_err = open(f'{resultDir}/{logdir}/{os.path.basename(zipFN)}_integrator_err.csv','w')
+    subprocess.call(os.path.expanduser("~/opt/MIDAS/FF_HEDM/bin/IntegratorZarr")+f' {zipFN}',shell=True,env=env,stdout=f,stderr=f_err)
+    finFN = f'{zipFN}.caked.hdf'
+    outzip = finFN+'.zip'
+    zipF = Path(outzip)
+    if zipF.exists():
+        shutil.move(outzip,outzip+'.old')
+    with fsspec.open(finFN,mode='rb', anon=False, requester_pays=True,default_fill_cache=False) as f:
+        storeZip = zarr.ZipStore(outzip)
+        h5chunkszip = Hdf5ToZarr(f, storeZip)
+        h5chunkszip.translate()
+        storeZip.close()
+    return outzip
+
 
 parser = MyParser(description='''Code to integrate files. Contact: hsharma@anl.gov''', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-resultFolder', type=str, required=False, default ='.', help='Folder where you want to save results.')
@@ -55,6 +81,7 @@ parser.add_argument('-startFileNr', type=int, required=False, default=-1, help='
 parser.add_argument('-endFileNr', type=int, required=False, default=-1, help='End fileNr. Default is -1, which means a single file is processed.')
 parser.add_argument('-convertFiles', type=int, required=False, default=1, help='Whether want to convert files to ZarrZip format or not.')
 parser.add_argument('-mapDetector', type=int, required=False, default=1, help='Whether want to generate map of detector or not. If unsure, put to 1. If already have the CORRECT Map.bin and nMap.bin, put it to 0.')
+parser.add_argument('-nCPUs', type=int, required=False, default=1, help='If you want to use multiple CPUs.')
 args, unparsed = parser.parse_known_args()
 resultDir = args.resultFolder
 psFN = args.paramFN
@@ -66,6 +93,7 @@ startFileNr = args.startFileNr
 endFileNr = args.endFileNr
 convertFiles = args.convertFiles
 mapDetector = args.mapDetector
+nCPUs = args.nCPUs
 
 if len(resultDir) == 0 or resultDir == '.':
     resultDir = os.getcwd()
@@ -86,7 +114,8 @@ if startFileNr == -1:
 if endFileNr == -1:
     endFileNr = startFileNr
 nrFiles = endFileNr - startFileNr + 1
-for fileNr in range(nrFiles):
+if mapDetector == 1:
+    fileNr = 0
     thisFN = InputFN.replace(startFileNrStr,str(startFileNr+fileNr).zfill(6))
     if convertFiles == 1:
         zipFN = generateZip(resultDir,psFN,dfn=thisFN,nchunks=numFrameChunks,preproc=preProc)
@@ -99,18 +128,37 @@ for fileNr in range(nrFiles):
         f = open(f'{resultDir}/{logdir}/map_out.csv','w')
         f_err = open(f'{resultDir}/{logdir}/map_err.csv','w')
         subprocess.call(os.path.expanduser("~/opt/MIDAS/FF_HEDM/bin/DetectorMapperZarr")+f' {zipFN}',shell=True,env=env,stdout=f,stderr=f_err)
-    f = open(f'{resultDir}/{logdir}/{os.path.basename(zipFN)}_integrator_out.csv','w')
-    f_err = open(f'{resultDir}/{logdir}/{os.path.basename(zipFN)}_integrator_err.csv','w')
-    subprocess.call(os.path.expanduser("~/opt/MIDAS/FF_HEDM/bin/IntegratorZarr")+f' {zipFN}',shell=True,env=env,stdout=f,stderr=f_err)
-    finFN = f'{zipFN}.caked.hdf'
-    outzip = finFN+'.zip'
-    zipF = Path(outzip)
-    if zipF.exists():
-        shutil.move(outzip,outzip+'.old')
-    with fsspec.open(finFN,mode='rb', anon=False, requester_pays=True,default_fill_cache=False) as f:
-        storeZip = zarr.ZipStore(outzip)
-        h5chunkszip = Hdf5ToZarr(f, storeZip)
-        h5chunkszip.translate()
-        storeZip.close()
-    print(f'Ouput file {outzip} tree structure:')
-    print(zarr.open(outzip).tree())
+
+# RUN THIS IN PARALLEL
+if nCPUs == 1:
+    for fileNr in range(1,nrFiles):
+        thisFN = InputFN.replace(startFileNrStr,str(startFileNr+fileNr).zfill(6))
+        if convertFiles == 1:
+            zipFN = generateZip(resultDir,psFN,dfn=thisFN,nchunks=numFrameChunks,preproc=preProc)
+        else:
+            if thisFN[-3:] != 'zip':
+                thisFN += '.analysis.MIDAS.zip'
+            zipFN = resultDir + thisFN
+            print(f'Processing file: {zipFN}')
+        f = open(f'{resultDir}/{logdir}/{os.path.basename(zipFN)}_integrator_out.csv','w')
+        f_err = open(f'{resultDir}/{logdir}/{os.path.basename(zipFN)}_integrator_err.csv','w')
+        subprocess.call(os.path.expanduser("~/opt/MIDAS/FF_HEDM/bin/IntegratorZarr")+f' {zipFN}',shell=True,env=env,stdout=f,stderr=f_err)
+        finFN = f'{zipFN}.caked.hdf'
+        outzip = finFN+'.zip'
+        zipF = Path(outzip)
+        if zipF.exists():
+            shutil.move(outzip,outzip+'.old')
+        with fsspec.open(finFN,mode='rb', anon=False, requester_pays=True,default_fill_cache=False) as f:
+            storeZip = zarr.ZipStore(outzip)
+            h5chunkszip = Hdf5ToZarr(f, storeZip)
+            h5chunkszip.translate()
+            storeZip.close()
+        print(f'Ouput file {outzip} tree structure:')
+        print(zarr.open(outzip).tree())
+else:
+    work_data = [fileNr for fileNr in range(1,nrFiles)]
+    p = Pool(nCPUs)
+    res = p.map(runOneFile,work_data)
+    for outzip in res:
+        print(f'Ouput file {outzip} tree structure:')
+        print(zarr.open(outzip).tree())
