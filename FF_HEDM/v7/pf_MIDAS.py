0b6451bfa275f6c3a71b80e9cf00bc0e2f54317a
Hemant Sharma <hemant@sharmas.biz>
2024-04-28 11:46:52 -0500

    Debug

--- FF_HEDM/v7/pf_MIDAS.py
+++ FF_HEDM/v7/pf_MIDAS.py
@@ -56,7 +56,9 @@ def peaks(resultDir,zipFN,numProcs,blockNr=0,numBlocks=1):
     f = open(f'{resultDir}/output/peaksearch_out{blockNr}.csv','w')
     f_err = open(f'{resultDir}/output/peaksearch_err{blockNr}.csv','w')
     f_err.write("I was able to do someting.")
-    subprocess.call(os.path.expanduser("~/opt/MIDAS/FF_HEDM/bin/PeaksFittingOMPZarr")+f' {zipFN} {blockNr} {numBlocks} {numProcs} {resultDir}',shell=True,env=env,stdout=f,stderr=f_err)
+    cmd_this = os.path.expanduser("~/opt/MIDAS/FF_HEDM/bin/PeaksFittingOMPZarr")+f' {zipFN} {blockNr} {numBlocks} {numProcs} {resultDir}'
+    f_err.write(cmd_this)
+    subprocess.call(cmd_this,shell=True,env=env,stdout=f,stderr=f_err)
     f.close()
     f_err.close()
 
@@ -70,7 +72,7 @@ def indexscanning(resultDir,numProcs,num_scans,blockNr=0,numBlocks=1):
     env['LD_LIBRARY_PATH'] = f'{midas_path}/BLOSC/lib64:{midas_path}/FFTW/lib:{midas_path}/HDF5/lib:{midas_path}/LIBTIFF/lib:{midas_path}/LIBZIP/lib64:{midas_path}/NLOPT/lib:{midas_path}/ZLIB/lib'
     f = open(f'{resultDir}/output/indexing_out{blockNr}.csv','w')
     f_err = open(f'{resultDir}/output/indexing_err{blockNr}.csv','w')
-    subprocess.call(os.path.expanduser("~/opt/MIDAS/FF_HEDM/bin/IndexerScanningOMP")+f' paramstest.txt {blockNr} {numBlocks} {num_scans} {numProcs}',shell=True,env=env,stdout=f,stderr=f_err)
+    subprocess.call(os.path.expanduser("~/opt/MIDAS/FF_HEDM/bin/IndexerScanningOMP")+f' paramstest.txt {numBlocks} {blockNr} {num_scans} {numProcs}',shell=True,env=env,stdout=f,stderr=f_err)
     f.close()
     f_err.close()
 
