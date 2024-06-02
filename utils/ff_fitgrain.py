6cf937204321f3265dd6c6c0d51d1c796818f2ec
Hemant Sharma <hemant@sharmas.biz>
2024-06-02 09:21:17 -0500

    Ability to choose which property to use for sorting grains for fitting tiltX.

--- utils/ff_fitgrain.py
+++ utils/ff_fitgrain.py
@@ -35,18 +35,26 @@ def main():
     parser.add_argument('-psFN', type=str, required=True, help='ps file that will be used. This is temporary. Will be replaced by zarr soon.')
     parser.add_argument('-fractionGrains', type=int, required=False, default=20, help='1/fractionGrains will be used optimized. Typically ~20.')
     parser.add_argument('-numProcs', type=int, required=False, default=8, help='Number of processors to use.')
+    parser.add_argument('-propertyToSortGrains', type=str, required=False, default='DiffOme', help='Which property to choose to sort grains, choose either DiffOme or DiffPos.')
     args, unparsed = parser.parse_known_args()
     Folder = args.resultFolder
     psFN = args.psFN
     frac = args.fractionGrains
     numProcs = args.numProcs
+    prop = args.propertyToSortGrains
+    if prop == 'DiffOme':
+        colVal = 20
+    elif prop == 'DiffPos':
+        colVal = 19
+    else: # Default DiffOme
+        colVal = 20
     os.makedirs(f'{Folder}/fitGrain/',exist_ok=True)
     GrainsFN = Folder + '/Grains.csv'
     os.chdir(Folder)
     subprocess.call(os.path.expanduser('~/opt/MIDAS/FF_HEDM/bin/GetHKLList')+f' {psFN}',shell=True,stdout=open('hklsout.csv','w'))
 
     grs = np.genfromtxt(GrainsFN,skip_header=9)
-    grsSort = grs[grs[:,20].argsort()]
+    grsSort = grs[grs[:,colVal].argsort()]
     nGrs = grsSort.shape[0]
     nSpotsToRefine = nGrs // frac # We will take 1/nth of grains and refine them
     IDs = list(tuple(grsSort[:nSpotsToRefine,0]))
