927fa4fe9ed0ac66ce1804e7f88fec0ab4d44f3a
Hemant Sharma <hemant@sharmas.biz>
2024-07-29 00:57:48 -0500

    Debug

--- utils/ffGenerateZip.py
+++ utils/ffGenerateZip.py
@@ -197,7 +197,8 @@ elif 'zip' in InputFN[-5:]:
     data_orig = zf[dataLoc]
     nFrames,numZ,numY = data_orig.shape
     data = exc.create_dataset('data',shape=(nFrames,numZ,numY),dtype=np.uint16,chunks=(1,numZ,numY),compression=compressor)
-    data[:] = data_orig[:]
+    zarr.copy(data,data_orig)
+    # data[:] = data_orig[:]
     darkData = np.zeros((10,numPxZ,numPxY))
     brightData = np.copy(darkData)
     dark = exc.create_dataset('dark',shape=darkData.shape,dtype=np.uint16,chunks=(1,darkData.shape[1],darkData.shape[2]),compression=compressor)
