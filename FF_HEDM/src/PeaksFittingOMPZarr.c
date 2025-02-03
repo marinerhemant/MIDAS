a8e0e09c0d5a29db6a4b92ddb9a46dc6e85a1446
Hemant Sharma <hemant@sharmas.biz>
2025-02-03 17:42:02 -0600

    Debug

--- FF_HEDM/src/PeaksFittingOMPZarr.c
+++ FF_HEDM/src/PeaksFittingOMPZarr.c
@@ -1102,7 +1102,7 @@ int main(int argc, char *argv[]){
     }
 	if (argc>5) resultFolder = argv[5];
 	int fitPeaks = 1;
-	if (argc>6) fitPeaks = int(argv[6]);
+	if (argc>6) fitPeaks = (int) argv[6];
 	int TransOpt[nImTransOpt], RingNrs[nRingsThresh];
     double Thresholds[nRingsThresh];
     // Read TransOpt
