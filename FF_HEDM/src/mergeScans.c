0113522fffa55814c91739548efd7703ea8d37b4
Hemant Sharma <hemant@sharmas.biz>
2025-02-05 12:36:17 -0600

    Debug

--- FF_HEDM/src/mergeScans.c
+++ FF_HEDM/src/mergeScans.c
@@ -149,7 +149,7 @@ int main(int argc, char *argv[]){
                     double recalcFrameNr = (180.0 + currentOmega)/(omegaStep*nFramesMerge);
                     frameToPut = (int)floor(recalcFrameNr);
                     if (frameToPut >= nFrames/nFramesMerge || frameToPut <0) printf("%lf %lf %lf %lf %d\n",thisOmega,delOmega,currentOmega,recalcFrameNr,frameToPut);
-                    if (frameToPut == nFrames/nFramesMerge) frameToPut=0;
+                    if (frameToPut >= nFrames/nFramesMerge) frameToPut -= nFrames/nFramesMerge;
                 } else frameToPut = frameNr / nFramesMerge;
                 dsz = blosc1_decompress(&allData[sizeArr[frameNr*2+1]],rawImage,dsz);
                 memcpy(ImageAsym,rawImage,dsz);
