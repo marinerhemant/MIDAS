48905ad0f0f4a223e99d64e6aa73c6d7904db629
Hemant Sharma <hemant@sharmas.biz>
2025-02-07 16:00:12 -0600

    Debug

--- FF_HEDM/src/findSingleSolutionPF.c
+++ FF_HEDM/src/findSingleSolutionPF.c
@@ -456,8 +456,8 @@ main(int argc, char *argv[])
                 locThis = i*maxNHKLs*nScans+j*nScans+k;
                 if (sinoArr[locThis] > 0){
                     sinoArr[locThis] /= maxIntThis;
-                    // sinoArr[locThis] *= -1;
-                    // sinoArr[locThis] = exp(sinoArr[locThis]);
+                    sinoArr[locThis] *= -1;
+                    sinoArr[locThis] = exp(sinoArr[locThis]);
                     AvAngle += allOmeArr[i*maxNHKLs*nScans+j*nScans+k];
                     nAngles++;
                 }
