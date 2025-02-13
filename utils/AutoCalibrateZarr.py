b28b0287c70e1d5bf862fa2d5a46e974c2017460
Hemant Sharma <hemant@sharmas.biz>
2025-02-12 21:30:54 -0600

    Debug

--- utils/AutoCalibrateZarr.py
+++ utils/AutoCalibrateZarr.py
@@ -80,6 +80,7 @@ LsdGuess = args.LsdGuess
 convertFile = args.ConvertFile
 
 if convertFile == 1 or convertFile == 2:
+	print(convertFile)
 	psFN = args.paramFN
 	if len(psFN) == 0:
 		print("Provide the parameter file if you want to generate a zarr zip file.")
