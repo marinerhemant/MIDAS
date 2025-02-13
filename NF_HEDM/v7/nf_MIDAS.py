ce9e130899d4c342d66949911860426869d87bfc
Hemant Sharma <hemant@sharmas.biz>
2025-02-13 00:28:36 -0600

    Debug

--- NF_HEDM/v7/nf_MIDAS.py
+++ NF_HEDM/v7/nf_MIDAS.py
@@ -205,9 +205,7 @@ if len(tomoFN) > 1:
     shutil.move('grid.txt','grid_unfilt.txt')
     shutil.move('gridNew.txt','grid.txt')
 elif len(GridMask) > 0:
-    print(GridMask)
     gridpoints = np.genfromtxt('grid.txt',skip_header=1,delimiter=' ')
-    print(gridpoints)
     gridpoints = gridpoints[gridpoints[:,2]>=GridMask[0],:]
     gridpoints = gridpoints[gridpoints[:,2]<=GridMask[1],:]
     gridpoints = gridpoints[gridpoints[:,3]>=GridMask[2],:]
