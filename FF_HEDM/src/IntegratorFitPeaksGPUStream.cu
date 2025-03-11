c47ac7cebeadf6d8adf3c804fdf027fb49f9b330
Hemant Sharma <hemant@sharmas.biz>
2025-03-11 16:02:16 -0500

    Debug

--- FF_HEDM/src/IntegratorFitPeaksGPUStream.cu
+++ FF_HEDM/src/IntegratorFitPeaksGPUStream.cu
@@ -808,7 +808,7 @@ int main(int argc, char *argv[]){
 		gpuErrchk(cudaMemset(devIntArrPerFrame,0,bigArrSize*sizeof(double)));
 		gpuErrchk(cudaMemcpy(devImage,Image,NrPixelsY*NrPixelsZ*sizeof(double),cudaMemcpyHostToDevice));
 		gpuErrchk(cudaDeviceSynchronize());
-		int tPB = 1024;
+		int tPB = 512;
 		int nrVox = (bigArrSize+tPB-1))/tPB;
 		if (mapMaskSize==0)
 			integrate_noMapMask <<<tPB,nrVox>>> (px,Lsd,bigArrSize,Normalize,sumImages,i,NrPixelsY, 
