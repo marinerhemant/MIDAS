abd3994add1f53560608f09bc815ed911b7c0ef6
Hemant Sharma <hemant@sharmas.biz>
2025-03-10 20:33:40 -0500

    Debug

--- FF_HEDM/src/Integrator.cu
+++ FF_HEDM/src/Integrator.cu
@@ -3,7 +3,7 @@
 // See LICENSE file.
 //
 // ~/opt/midascuda/cuda/bin/nvcc integrator.cu -o integrator -Xcompiler -g -arch sm_90 -gencode=arch=compute_90,code=sm_90 -I/scratch/s1iduser/sharma_tests/HDF5/include -L/scratch/s1iduser/sharma_tests/HDF5/lib -lhdf5_hl -lhdf5 -I/scratch/s1iduser/sharma_tests/LIBTIFF/include -L/scratch/s1iduser/sharma_tests/LIBTIFF/lib -ltiff -O3
-// ~/opt/midascuda/cuda/bin/nvcc src/Integrator.cu -o ../bin/IntegratorGPU -Xcompiler -g -arch sm_90 -gencode=arch=compute_90,code=sm_90 -I/home/beams/S1IDUSER/.MIDAS/HDF5/include -L/home/beams/S1IDUSER/.MIDAS/HDF5/lib -lhdf5_hl -lhdf5 -I/home/beams/S1IDUSER/.MIDAS/LIBTIFF/include -L/home/beams/S1IDUSER/.MIDAS/LIBTIFF/lib -ltiff -O3
+// ~/opt/midascuda/cuda/bin/nvcc src/Integrator.cu -o bin/IntegratorGPU -Xcompiler -g -arch sm_90 -gencode=arch=compute_90,code=sm_90 -I/home/beams/S1IDUSER/.MIDAS/HDF5/include -L/home/beams/S1IDUSER/.MIDAS/HDF5/lib -lhdf5_hl -lhdf5 -I/home/beams/S1IDUSER/.MIDAS/LIBTIFF/include -L/home/beams/S1IDUSER/.MIDAS/LIBTIFF/lib -ltiff -O3
 // ~/opt/midascuda/cuda_RHEL8/bin/nvcc src/Integrator.cu -o bin/IntegratorGPU -Xcompiler -g -arch sm_90 -gencode=arch=compute_90,code=sm_90 -I/home/beams/S1IDUSER/.MIDAS/HDF5/include -L/home/beams/S1IDUSER/.MIDAS/HDF5/lib -lhdf5_hl -lhdf5 -I/home/beams/S1IDUSER/.MIDAS/LIBTIFF/include -L/home/beams/S1IDUSER/.MIDAS/LIBTIFF/lib -ltiff -O3
 // export LD_LIBRARY_PATH=/scratch/s1iduser/sharma_tests/HDF5/lib:/scratch/s1iduser/sharma_tests/LIBTIFF/lib:$LD_LIBRARY_PATH
 // export LD_LIBRARY_PATH=/home/beams/S1IDUSER/.MIDAS/HDF5/lib:/home/beams/S1IDUSER/.MIDAS/LIBTIFF/lib:$LD_LIBRARY_PATH
