710d43d37b4966423afe97ecca189b0c7c1fbeeb
Hemant Sharma <hemant@sharmas.biz>
2025-03-10 20:32:19 -0500

    Debug

--- FF_HEDM/src/Integrator.cu
+++ FF_HEDM/src/Integrator.cu
@@ -3,7 +3,7 @@
 // See LICENSE file.
 //
 // ~/opt/midascuda/cuda/bin/nvcc integrator.cu -o integrator -Xcompiler -g -arch sm_90 -gencode=arch=compute_90,code=sm_90 -I/scratch/s1iduser/sharma_tests/HDF5/include -L/scratch/s1iduser/sharma_tests/HDF5/lib -lhdf5_hl -lhdf5 -I/scratch/s1iduser/sharma_tests/LIBTIFF/include -L/scratch/s1iduser/sharma_tests/LIBTIFF/lib -ltiff -O3
-// ~/opt/midascuda/cuda/bin/nvcc Integrator.cu midas_cuda_client.c -o ../bin/IntegratorGPU -Xcompiler -g -arch sm_90 -gencode=arch=compute_90,code=sm_90 -I/home/beams/S1IDUSER/.MIDAS/HDF5/include -L/home/beams/S1IDUSER/.MIDAS/HDF5/lib -lhdf5_hl -lhdf5 -I/home/beams/S1IDUSER/.MIDAS/LIBTIFF/include -L/home/beams/S1IDUSER/.MIDAS/LIBTIFF/lib -ltiff -O3
+// ~/opt/midascuda/cuda/bin/nvcc src/Integrator.cu -o ../bin/IntegratorGPU -Xcompiler -g -arch sm_90 -gencode=arch=compute_90,code=sm_90 -I/home/beams/S1IDUSER/.MIDAS/HDF5/include -L/home/beams/S1IDUSER/.MIDAS/HDF5/lib -lhdf5_hl -lhdf5 -I/home/beams/S1IDUSER/.MIDAS/LIBTIFF/include -L/home/beams/S1IDUSER/.MIDAS/LIBTIFF/lib -ltiff -O3
 // ~/opt/midascuda/cuda_RHEL8/bin/nvcc src/Integrator.cu -o bin/IntegratorGPU -Xcompiler -g -arch sm_90 -gencode=arch=compute_90,code=sm_90 -I/home/beams/S1IDUSER/.MIDAS/HDF5/include -L/home/beams/S1IDUSER/.MIDAS/HDF5/lib -lhdf5_hl -lhdf5 -I/home/beams/S1IDUSER/.MIDAS/LIBTIFF/include -L/home/beams/S1IDUSER/.MIDAS/LIBTIFF/lib -ltiff -O3
 // export LD_LIBRARY_PATH=/scratch/s1iduser/sharma_tests/HDF5/lib:/scratch/s1iduser/sharma_tests/LIBTIFF/lib:$LD_LIBRARY_PATH
 // export LD_LIBRARY_PATH=/home/beams/S1IDUSER/.MIDAS/HDF5/lib:/home/beams/S1IDUSER/.MIDAS/LIBTIFF/lib:$LD_LIBRARY_PATH
@@ -41,7 +41,63 @@
 #include <hdf5_hl.h>
 #include <assert.h>
 #include <cuda.h>
-#include "midas_cuda_client.h"
+#include <sys/socket.h>
+
+int cuda_client_init();
+int cuda_execute(const char* command);
+void cuda_client_close();
+
+#define SOCKET_PATH "/tmp/cuda_server_socket"
+
+static int client_fd = -1;
+
+int cuda_client_init() {
+    client_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (client_fd < 0) {
+        perror("Socket creation failed");
+        return -1;
+    }
+    
+    struct sockaddr_un address;
+    memset(&address, 0, sizeof(struct sockaddr_un));
+    address.sun_family = AF_UNIX;
+    strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path) - 1);
+    
+    if (connect(client_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Connection failed");
+        close(client_fd);
+        client_fd = -1;
+        return -1;
+    }
+    
+    return 0;
+}
+
+int cuda_execute(const char* command) {
+    if (client_fd < 0) {
+        fprintf(stderr, "Not connected to CUDA server\n");
+        return -1;
+    }
+    
+    write(client_fd, command, strlen(command));
+    
+    char buffer[256];
+    int bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+    if (bytes_read > 0) {
+        buffer[bytes_read] = '\0';
+        printf("Server response: %s\n", buffer);
+        return 0;
+    }
+    
+    return -1;
+}
+
+void cuda_client_close() {
+    if (client_fd >= 0) {
+        close(client_fd);
+        client_fd = -1;
+    }
+}
 
 typedef double pixelvalue;
 
