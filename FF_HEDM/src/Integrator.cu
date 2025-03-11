c598c12ae0974b2c17344a8eaf5b47610adb3f34
Hemant Sharma <hemant@sharmas.biz>
2025-03-10 20:55:59 -0500

    Debug

--- FF_HEDM/src/Integrator.cu
+++ FF_HEDM/src/Integrator.cu
@@ -41,65 +41,6 @@
 #include <hdf5_hl.h>
 #include <assert.h>
 #include <cuda.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-
-int cuda_client_init();
-int cuda_execute(const char* command);
-void cuda_client_close();
-
-#define SOCKET_PATH "/tmp/cuda_server_socket"
-
-static int client_fd = -1;
-
-int cuda_client_init() {
-    client_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (client_fd < 0) {
-        perror("Socket creation failed");
-        return -1;
-    }
-    
-    struct sockaddr_un address;
-    memset(&address, 0, sizeof(struct sockaddr_un));
-    address.sun_family = AF_UNIX;
-    strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path) - 1);
-    
-	if (connect(client_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-		perror("Connection failed");
-		fprintf(stderr, "Trying to connect to: %s\n", SOCKET_PATH);
-		close(client_fd);
-		client_fd = -1;
-		return -1;
-	}
-		
-    return 0;
-}
-
-int cuda_execute(const char* command) {
-    if (client_fd < 0) {
-        fprintf(stderr, "Not connected to CUDA server\n");
-        return -1;
-    }
-    
-    write(client_fd, command, strlen(command));
-    
-    char buffer[256];
-    int bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
-    if (bytes_read > 0) {
-        buffer[bytes_read] = '\0';
-        printf("Server response: %s\n", buffer);
-        return 0;
-    }
-    
-    return -1;
-}
-
-void cuda_client_close() {
-    if (client_fd >= 0) {
-        close(client_fd);
-        client_fd = -1;
-    }
-}
 
 typedef double pixelvalue;
 
---
+++ FF_HEDM/src/midas_cuda_daemon.c
@@ -0,0 +1,33 @@
+// cuda_daemon.c
+// ~/opt/midascuda/cuda/bin/nvcc -o cuda_daemon cuda_daemon.c -I/usr/local/cuda/include -L/usr/local/cuda/lib64 -lcudart
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <cuda_runtime.h>
+
+int main(char *argv[], int argc) {
+    if (argc == 2) {
+        // If the user passes an argument, use it as the CUDA device
+        int device = atoi(argv[1]);
+        cudaSetDevice(device);
+    } else{
+        // Otherwise, use the default device
+        cudaSetDevice(0);
+    }
+    
+    // Create a small file to indicate the daemon is running
+    FILE *f = fopen("/tmp/cuda_daemon_running", "w");
+    if (f) {
+        fprintf(f, "%d", getpid());
+        fclose(f);
+    }
+    
+    printf("CUDA device initialized. Daemon running with PID %d\n", getpid());
+    
+    // Sleep forever, keeping the CUDA context alive
+    while(1) {
+        sleep(3600);  // Sleep for an hour and repeat
+    }
+    
+    return 0;
+}
