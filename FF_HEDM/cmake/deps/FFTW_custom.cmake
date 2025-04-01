d4ac39f571d8b8873da353fdb6a6819231a9d597
Hemant Sharma <hemant@sharmas.biz>
2025-04-01 16:43:12 -0500

    Debug

--- FF_HEDM/cmake/deps/FFTW_custom.cmake
+++ FF_HEDM/cmake/deps/FFTW_custom.cmake
@@ -19,11 +19,14 @@ if(NOT fftw_POPULATED)
   
   set(FFTW_OPTIONS --enable-float --disable-fortran --enable-sse --enable-sse2 --enable-avx --enable-avx2 --enable-avx-128-fma --enable-generic-simd128 --enable-generic-simd256)
   
+  # Define the installation directory
+  set(FFTW_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/fftw_install)
+  
   ExternalProject_Add(fftw_external
     SOURCE_DIR ${fftw_SOURCE_DIR}
     BINARY_DIR ${fftw_BINARY_DIR}
     CONFIGURE_COMMAND ${fftw_SOURCE_DIR}/configure 
-                      --prefix=${CMAKE_BINARY_DIR}/deps
+                      --prefix=${FFTW_INSTALL_DIR}
                       ${FFTW_SHARED_ARG}
                       ${FFTW_OPTIONS}
     BUILD_COMMAND make -j8
@@ -31,21 +34,29 @@ if(NOT fftw_POPULATED)
     BUILD_ALWAYS OFF
   )
   
+  # Create include directory to avoid errors during configuration
+  file(MAKE_DIRECTORY ${FFTW_INSTALL_DIR}/include)
+  
   # Create interface library
-  add_library(FFTW::FFTW INTERFACE IMPORTED)
+  add_library(FFTW::FFTW INTERFACE IMPORTED GLOBAL)
   set_target_properties(FFTW::FFTW PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/deps/include"
+    INTERFACE_INCLUDE_DIRECTORIES "${FFTW_INSTALL_DIR}/include"
   )
   
+  # Set up library paths based on build type
   if(BUILD_SHARED_LIBS)
-    set_target_properties(FFTW::FFTW PROPERTIES
-      INTERFACE_LINK_LIBRARIES "${CMAKE_BINARY_DIR}/deps/lib/libfftw3f${CMAKE_SHARED_LIBRARY_SUFFIX}"
-    )
+    if(APPLE)
+      set(FFTW_LIB_PATH "${FFTW_INSTALL_DIR}/lib/libfftw3f.dylib")
+    else()
+      set(FFTW_LIB_PATH "${FFTW_INSTALL_DIR}/lib/libfftw3f.so")
+    endif()
   else()
-    set_target_properties(FFTW::FFTW PROPERTIES
-      INTERFACE_LINK_LIBRARIES "${CMAKE_BINARY_DIR}/deps/lib/libfftw3f${CMAKE_STATIC_LIBRARY_SUFFIX}"
-    )
+    set(FFTW_LIB_PATH "${FFTW_INSTALL_DIR}/lib/libfftw3f.a")
   endif()
+  
+  set_target_properties(FFTW::FFTW PROPERTIES
+    INTERFACE_LINK_LIBRARIES "${FFTW_LIB_PATH}"
+  )
   
   # Add dependency
   add_dependencies(FFTW::FFTW fftw_external)
