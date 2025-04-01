ea79ecb9f673d26208e4e0b6fec550ef6b4da4d2
Hemant Sharma <hemant@sharmas.biz>
2025-04-01 15:33:02 -0500

    Debug

--- FF_HEDM/cmake/deps/BLOSC_custom.cmake
+++ FF_HEDM/cmake/deps/BLOSC_custom.cmake
@@ -1,77 +1,47 @@
 FetchContent_Declare(
-  blosc
-  GIT_REPOSITORY https://github.com/Blosc/c-blosc2.git
+  blosc1
+  GIT_REPOSITORY https://github.com/Blosc/c-blosc.git
   GIT_TAG main
 )
 
-FetchContent_GetProperties(blosc)
-if(NOT blosc_POPULATED)
-  FetchContent_Populate(blosc)
+FetchContent_GetProperties(blosc1)
+if(NOT blosc1_POPULATED)
+  FetchContent_Populate(blosc1)
   
-  # Rename the internal zlib to avoid conflict
-  if(EXISTS "${blosc_SOURCE_DIR}/internal-complibs/zlib-ng-2.0.7/CMakeLists.txt")
-    file(READ "${blosc_SOURCE_DIR}/internal-complibs/zlib-ng-2.0.7/CMakeLists.txt" ZLIB_CMAKE_CONTENT)
-    
-    # Replace the target name "zlib" with "blosc_internal_zlib" in all places
-    string(REPLACE "add_library(zlib" 
-                   "add_library(blosc_internal_zlib" 
-                   ZLIB_CMAKE_CONTENT "${ZLIB_CMAKE_CONTENT}")
-    
-    # Replace any references to "zlib" target with "blosc_internal_zlib"
-    string(REPLACE "TARGET zlib" 
-                   "TARGET blosc_internal_zlib" 
-                   ZLIB_CMAKE_CONTENT "${ZLIB_CMAKE_CONTENT}")
-    
-    # Update the output name to avoid filename conflicts
-    string(REPLACE "OUTPUT_NAME z" 
-                   "OUTPUT_NAME blosc_z" 
-                   ZLIB_CMAKE_CONTENT "${ZLIB_CMAKE_CONTENT}")
-    
-    file(WRITE "${blosc_SOURCE_DIR}/internal-complibs/zlib-ng-2.0.7/CMakeLists.txt" "${ZLIB_CMAKE_CONTENT}")
-  endif()
-  
-  # Also update BLOSC's main CMakeLists.txt to use the renamed zlib target
-  if(EXISTS "${blosc_SOURCE_DIR}/CMakeLists.txt")
-    file(READ "${blosc_SOURCE_DIR}/CMakeLists.txt" BLOSC_CMAKE_CONTENT)
-    
-    # Replace references to "zlib" with "blosc_internal_zlib" 
-    string(REPLACE "zlib " 
-                   "blosc_internal_zlib " 
-                   BLOSC_CMAKE_CONTENT "${BLOSC_CMAKE_CONTENT}")
-    
-    string(REPLACE "zlib;" 
-                   "blosc_internal_zlib;" 
-                   BLOSC_CMAKE_CONTENT "${BLOSC_CMAKE_CONTENT}")
-    
-    file(WRITE "${blosc_SOURCE_DIR}/CMakeLists.txt" "${BLOSC_CMAKE_CONTENT}")
-  endif()
-  
-  # Set BLOSC options
+  # Set BLOSC1 options
   set(BLOSC_IS_SUBPROJECT ON CACHE BOOL "Build as subproject" FORCE)
   set(BLOSC_INSTALL OFF CACHE BOOL "Install blosc" FORCE)
-  set(BLOSC_SHARED ${BUILD_SHARED_LIBS} CACHE BOOL "Build shared" FORCE)
-  set(BLOSC_STATIC NOT ${BUILD_SHARED_LIBS} CACHE BOOL "Build static" FORCE)
-  set(BLOSC_BUILD_TESTS OFF CACHE BOOL "Build tests" FORCE)
-  set(BLOSC_BUILD_BENCHMARKS OFF CACHE BOOL "Build benchmarks" FORCE)
-  set(BLOSC_BUILD_EXAMPLES OFF CACHE BOOL "Build examples" FORCE)
+  set(BUILD_SHARED ${BUILD_SHARED_LIBS} CACHE BOOL "Build shared" FORCE)
+  set(BUILD_STATIC NOT ${BUILD_SHARED_LIBS} CACHE BOOL "Build static" FORCE)
+  set(BUILD_TESTS OFF CACHE BOOL "Build tests" FORCE)
+  set(BUILD_BENCHMARKS OFF CACHE BOOL "Build benchmarks" FORCE)
+  set(BUILD_EXAMPLES OFF CACHE BOOL "Build examples" FORCE)
+  # The simple fix is to set BUILD_FUZZERS without the description
+  set(BUILD_FUZZERS OFF CACHE BOOL "Build fuzzers" FORCE)
   
-  # Disable external zlib to use internal one
-  set(BLOSC_PREFER_EXTERNAL_ZLIB OFF CACHE BOOL "Use external ZLIB" FORCE)
+  # Don't disable zlib - comment out or remove this check
+  # if(TARGET zlib)
+  #   message(STATUS "zlib target already exists, disabling internal zlib in BLOSC1")
+  #   set(DEACTIVATE_ZLIB ON CACHE BOOL "Do not include support for the Zlib library." FORCE)
+  # endif()
   
-  add_subdirectory(${blosc_SOURCE_DIR} ${blosc_BINARY_DIR})
+  # Avoid issues with other dependencies
+  set(DEACTIVATE_SNAPPY ON CACHE BOOL "Do not include support for the Snappy library." FORCE)
   
-  # Export BLOSC as a target
-  add_library(BLOSC::BLOSC INTERFACE IMPORTED)
+  add_subdirectory(${blosc1_SOURCE_DIR} ${blosc1_BINARY_DIR})
+  
+  # Export BLOSC1 as a target
+  add_library(BLOSC1::BLOSC1 INTERFACE IMPORTED)
   if(BUILD_SHARED_LIBS)
-    set_target_properties(BLOSC::BLOSC PROPERTIES
-      INTERFACE_LINK_LIBRARIES blosc2_shared
+    set_target_properties(BLOSC1::BLOSC1 PROPERTIES
+      INTERFACE_LINK_LIBRARIES blosc_shared
     )
   else()
-    set_target_properties(BLOSC::BLOSC PROPERTIES
-      INTERFACE_LINK_LIBRARIES blosc2_static
+    set_target_properties(BLOSC1::BLOSC1 PROPERTIES
+      INTERFACE_LINK_LIBRARIES blosc_static
     )
   endif()
-  set_target_properties(BLOSC::BLOSC PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${blosc_SOURCE_DIR}/include"
+  set_target_properties(BLOSC1::BLOSC1 PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${blosc1_SOURCE_DIR}/blosc"
   )
 endif()
