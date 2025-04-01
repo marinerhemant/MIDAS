ae13bc279da2fb8b1f331908f04c8ecdd961a6ea
Hemant Sharma <hemant@sharmas.biz>
2025-04-01 16:23:27 -0500

    debug

--- FF_HEDM/cmake/deps/NLOPT_custom.cmake
+++ FF_HEDM/cmake/deps/NLOPT_custom.cmake
@@ -1,61 +1,39 @@
 FetchContent_Declare(
   nlopt
-  URL https://www.dropbox.com/scl/fi/ux4ccf23z7rotkgbqbrmk/nlopt-2.4.2.tar.gz?rlkey=afq6l6yyu9fnw1hpe62l4gwqq&dl=1
-  URL_HASH MD5=d0b8f139a4acf29b76dbae69ade8ac54
+  GIT_REPOSITORY https://github.com/stevengj/nlopt.git
+  GIT_TAG master
 )
 
 FetchContent_GetProperties(nlopt)
 if(NOT nlopt_POPULATED)
   FetchContent_Populate(nlopt)
   
-  # Set up NLOPT build with autotools
-  set(NLOPT_SHARED_ARG)
-  if(BUILD_SHARED_LIBS)
-    set(NLOPT_SHARED_ARG --enable-shared --disable-static)
-  else()
-    set(NLOPT_SHARED_ARG --disable-shared --enable-static)
-  endif()
+  # Set up NLOPT build with CMake
+  set(NLOPT_CXX OFF CACHE BOOL "disable C++ routines")
+  set(NLOPT_PYTHON OFF CACHE BOOL "disable Python bindings")
+  set(NLOPT_OCTAVE OFF CACHE BOOL "disable Octave bindings")
+  set(NLOPT_MATLAB OFF CACHE BOOL "disable Matlab bindings")
+  set(NLOPT_GUILE OFF CACHE BOOL "disable Guile bindings")
+  set(NLOPT_SWIG OFF CACHE BOOL "disable SWIG bindings")
+  set(NLOPT_TESTS OFF CACHE BOOL "disable tests")
+  set(BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
   
-  ExternalProject_Add(nlopt_external
-    SOURCE_DIR ${nlopt_SOURCE_DIR}
-    BINARY_DIR ${nlopt_BINARY_DIR}
-    CONFIGURE_COMMAND ${nlopt_SOURCE_DIR}/configure 
-                      --prefix=${CMAKE_BINARY_DIR}/deps
-                      ${NLOPT_SHARED_ARG}
-                      --disable-python
-                      --disable-octave
-                      --disable-matlab
-                      --disable-guile
-    BUILD_COMMAND make -j8
-    INSTALL_COMMAND make install
-    BUILD_ALWAYS OFF
-  )
+  # Use CMake for building instead of autotools
+  add_subdirectory(${nlopt_SOURCE_DIR} ${nlopt_BINARY_DIR})
   
   # Create interface library
   add_library(NLOPT::NLOPT INTERFACE IMPORTED GLOBAL)
   
   # Set up include directories
   set_target_properties(NLOPT::NLOPT PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/deps/include"
+    INTERFACE_INCLUDE_DIRECTORIES "${nlopt_SOURCE_DIR}/src/api;${nlopt_BINARY_DIR}/src/api"
   )
   
-  # Set up link libraries based on build type
-  if(BUILD_SHARED_LIBS)
-    if(APPLE)
-      set_target_properties(NLOPT::NLOPT PROPERTIES
-        INTERFACE_LINK_LIBRARIES "${CMAKE_BINARY_DIR}/deps/lib/libnlopt.dylib"
-      )
-    else()
-      set_target_properties(NLOPT::NLOPT PROPERTIES
-        INTERFACE_LINK_LIBRARIES "${CMAKE_BINARY_DIR}/deps/lib/libnlopt.so"
-      )
-    endif()
-  else()
-    set_target_properties(NLOPT::NLOPT PROPERTIES
-      INTERFACE_LINK_LIBRARIES "${CMAKE_BINARY_DIR}/deps/lib/libnlopt.a"
-    )
-  endif()
+  # NLOpt now provides its own target we can link to
+  set_target_properties(NLOPT::NLOPT PROPERTIES
+    INTERFACE_LINK_LIBRARIES "nlopt"
+  )
   
   # Add dependency
-  add_dependencies(NLOPT::NLOPT nlopt_external)
+  add_dependencies(NLOPT::NLOPT nlopt)
 endif()
