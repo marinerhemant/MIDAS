7e32a7b2566a64ae7d887aef290204bde5561051
Hemant Sharma <hemant@sharmas.biz>
2025-04-01 14:54:21 -0500

    Debug

---
+++ FF_HEDM/cmake/deps/BLOSC_custom.cmake
@@ -0,0 +1,41 @@
+FetchContent_Declare(
+  blosc
+  GIT_REPOSITORY https://github.com/Blosc/c-blosc2.git
+  GIT_TAG main
+)
+
+FetchContent_GetProperties(blosc)
+if(NOT blosc_POPULATED)
+  FetchContent_Populate(blosc)
+  
+  # Set BLOSC options
+  set(BLOSC_IS_SUBPROJECT ON CACHE BOOL "Build as subproject" FORCE)
+  set(BLOSC_INSTALL OFF CACHE BOOL "Install blosc" FORCE)
+  set(BLOSC_SHARED ${BUILD_SHARED_LIBS} CACHE BOOL "Build shared" FORCE)
+  set(BLOSC_STATIC NOT ${BUILD_SHARED_LIBS} CACHE BOOL "Build static" FORCE)
+  set(BLOSC_BUILD_TESTS OFF CACHE BOOL "Build tests" FORCE)
+  set(BLOSC_BUILD_BENCHMARKS OFF CACHE BOOL "Build benchmarks" FORCE)
+  set(BLOSC_BUILD_EXAMPLES OFF CACHE BOOL "Build examples" FORCE)
+  
+  # Disable internal compression libraries to avoid conflicts
+  set(BLOSC_PREFER_EXTERNAL_ZLIB ON CACHE BOOL "Use external ZLIB" FORCE)
+  set(BLOSC_PREFER_EXTERNAL_LZ4 ON CACHE BOOL "Use external LZ4" FORCE)
+  set(BLOSC_PREFER_EXTERNAL_ZSTD ON CACHE BOOL "Use external ZSTD" FORCE)
+  
+  add_subdirectory(${blosc_SOURCE_DIR} ${blosc_BINARY_DIR})
+  
+  # Export BLOSC as a target
+  add_library(BLOSC::BLOSC INTERFACE IMPORTED)
+  if(BUILD_SHARED_LIBS)
+    set_target_properties(BLOSC::BLOSC PROPERTIES
+      INTERFACE_LINK_LIBRARIES blosc2_shared
+    )
+  else()
+    set_target_properties(BLOSC::BLOSC PROPERTIES
+      INTERFACE_LINK_LIBRARIES blosc2_static
+    )
+  endif()
+  set_target_properties(BLOSC::BLOSC PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${blosc_SOURCE_DIR}/include"
+  )
+endif()
