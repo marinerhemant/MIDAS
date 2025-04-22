d48e712bd695a0ece41792f6d85920861c5f910f
Hemant Sharma <hemant@sharmas.biz>
2025-04-22 11:02:51 -0500

    Debug

--- FF_HEDM/src/IntegratorFitPeaksGPUStream.cu
+++ FF_HEDM/src/IntegratorFitPeaksGPUStream.cu
@@ -1164,8 +1164,8 @@ int main(int argc, char *argv[]){
 		perror("Error opening output files");
 		exit(EXIT_FAILURE);
 	}
-	clock_t t1, t2, tIntegration, tFit, tFWLineout, tFWFitResult, tinter, tfin, tcopy1in, tcopy1out, tcopy2in, tcopy2out,twrite2din,twrite2dout,tmake1din,tmake1dout;
-	double diffT=0, diffInteg, diffWriteLineout, diffTFit, diffWriteFitResult, diffTinter, diffTcopy1, diffTcopy2, diffTwrite2d, diffTmake1d;
+	clock_t t1, t1inter, t2, tIntegration, tFit, tFWLineout, tFWFitResult, tinter, tfin, tcopy1in, tcopy1out, tcopy2in, tcopy2out,twrite2din,twrite2dout,tmake1din,tmake1dout;
+	double diffT=0, diffInteg, diffWriteLineout, diffTFit, diffWriteFitResult, diffTinter, diffTcopy1, diffTcopy2, diffTwrite2d, diffTmake1d, diffTsetup;
 	int firstFrame = 1;
     // Allocate arrays once before the loop
     double *int1D = (double *)calloc(nRBins, sizeof(*int1D));
@@ -1180,9 +1180,9 @@ int main(int argc, char *argv[]){
 
     // Main processing loop
     while (1) {
+		t1 = clock();
         DataChunk chunk;
         queue_pop(&process_queue, &chunk);
-		t1 = clock();
         // Process the data
         memcpy(ImageInT,chunk.data,chunk.size*BYTES_PER_PIXEL);
 		if ((NrTransOpt==0) || (NrTransOpt==1 && TransOpt[0]==0)){
@@ -1201,6 +1201,7 @@ int main(int argc, char *argv[]){
 				Image[j] = (double)ImageIn[j] - AverageDark[j];
 			}
 		}
+		t1inter = clock();
 		tcopy1in = clock();
 		gpuErrchk(cudaMemset(devIntArrPerFrame,0,bigArrSize*sizeof(double)));
 		gpuErrchk(cudaMemcpy(devImage,Image,NrPixelsY*NrPixelsZ*sizeof(double),cudaMemcpyHostToDevice));
@@ -1430,6 +1431,7 @@ int main(int argc, char *argv[]){
 
 		t2 = clock();
 		diffT = ((double)(t2-t1))/CLOCKS_PER_SEC;
+		diffTsetup = ((double)(t1inter-t1))/CLOCKS_PER_SEC;
 		diffTinter = ((double)(tfin-tinter))/CLOCKS_PER_SEC;
 		diffInteg = ((double)(tIntegration - t1))/CLOCKS_PER_SEC;
 		diffWriteLineout = ((double)(tFWLineout - tIntegration))/CLOCKS_PER_SEC;
@@ -1440,7 +1442,7 @@ int main(int argc, char *argv[]){
 		diffTwrite2d = ((double)(twrite2dout - twrite2din))/CLOCKS_PER_SEC;
 		diffTmake1d = ((double)(tmake1dout - tmake1din))/CLOCKS_PER_SEC;
 
-		printf("Did integration, total time: %lf s for this frame, frameNr: %d. %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",diffT,chunk.dataset_num,diffTinter,diffTcopy1,diffTcopy2,diffTwrite2d,diffTmake1d,diffInteg,diffWriteLineout,diffTFit,diffWriteFitResult);
+		printf("Did integration, total time: %lf s for this frame, frameNr: %d. %lf %lf %lf %lf %lf %lf %lf %lf %lf\n",diffT,chunk.dataset_num,diffTcopy1,diffTinter,diffTcopy2,diffTwrite2d,diffTmake1d,diffInteg,diffWriteLineout,diffTFit,diffWriteFitResult);
         free(chunk.data);
     }
     
