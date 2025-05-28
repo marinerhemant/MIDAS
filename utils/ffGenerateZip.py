50908410e4ff3a5ad0b45ed58dc1683f39912361
Hemant Sharma <hemant@sharmas.biz>
2025-05-28 11:01:19 -0500

    Debug

--- utils/ffGenerateZip.py
+++ utils/ffGenerateZip.py
@@ -209,7 +209,7 @@ if h5py.is_hdf5(InputFN):
         else:
             darkData = np.zeros((10,numZ,numY))
     print(darkData.shape)
-    dark = exc.create_dataset('dark',shape=darkData.shape,dtype=np.uint16,chunks=(1,darkData.shape[1],darkData.shape[2]),compression=compressor)
+    dark = exc.create_array('dark',shape=darkData.shape,dtype=np.uint16,chunks=(1,darkData.shape[1],darkData.shape[2]),compression=compressor)
     darkMean = np.mean(darkData[skipF:,:,:],axis=0).astype(np.uint16)
     if preProc!=-1:
         darkpreProc = darkMean + preProc
