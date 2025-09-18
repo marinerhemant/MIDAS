a7baa6b66ead61160d94947002ba385f4977d05c
Hemant Sharma <hemant@sharmas.biz>
2025-09-18 13:28:33 -0500

    Debug

--- FF_HEDM/src/FitPosOrStrainsOMP.c
+++ FF_HEDM/src/FitPosOrStrainsOMP.c
@@ -1809,7 +1809,7 @@ int main(int argc, char *argv[])
 				if (nSpotsData2 != 0){
 					spotPosAllSpots2 = mapDatasets[spotPosAllSpots];
 					if (spotPosAllSpots2 < 0 || spotPosAllSpots2 >= (size_t)nSpotsData2){
-						printf("SpotID %d mapped to invalid spot in dataset 2 (%zu). Skipping this spot.\n", (int)spotIDS[i], spotPosAllSpots2);
+						printf("SpotID %d mapped to invalid spot in dataset 2 (%zu). Will use original spot in this case.\n", (int)spotIDS[i], spotPosAllSpots2);
 						for (j=0;j<8;j++){
 							spotsYZO2[i][j] = spotsYZO[i][j];
 						}
