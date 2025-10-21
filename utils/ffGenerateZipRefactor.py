92a0552fed1d6784c02921a606d12311c0afa277
Hemant Sharma <hemant@sharmas.biz>
2025-10-22 05:18:25 +0530

    Refactor zip generation to use tifffile for TIFF

--- utils/ffGenerateZipRefactor.py
+++ utils/ffGenerateZipRefactor.py
@@ -349,6 +349,8 @@ def process_multifile_scan(file_type, config, z_groups):
 
     # The threshold for clipping is the dark mean plus the optional threshold value
     pre_proc_threshold = dark_mean + (pre_proc_thresh_value if pre_proc_thresh_value != -1 else 0)
+    if file_type != 'ge':
+        import tifffile
 
     for i in range(num_files):
         current_nr_str = str(start_nr + i).zfill(len(fNr_orig_str))
@@ -387,7 +389,7 @@ def process_multifile_scan(file_type, config, z_groups):
                 z_data[z_offset : z_offset + len(data_chunk)] = data_chunk
                 z_offset += len(data_chunk)
         else: # TIFF logic remains simple
-            data_chunk = np.fromfile(current_fn, dtype=output_dtype, offset=8).reshape((1, numPxZ, numPxY))
+            data_chunk = tifffile.imread(current_fn)
             if correction_active:
                 data_chunk = apply_correction(data_chunk, dark_mean, pre_proc_threshold)
             z_data[z_offset : z_offset + len(data_chunk)] = data_chunk
