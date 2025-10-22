87c8fbee37d586f91dd31de870c80929c2ed7f78
Hemant Sharma <hemant@sharmas.biz>
2025-10-22 06:20:29 +0530

    Improve error handling in Zarr data type reading
    Added error handling for reading Zarr data type.

--- FF_HEDM/src/PeaksFittingOMPZarrRefactor.c
+++ FF_HEDM/src/PeaksFittingOMPZarrRefactor.c
@@ -1751,11 +1751,12 @@ static ErrorCode readZarrDataType(
         if (strstr(fileInfo.name, "measurement/process/scan_parameters/datatype") != NULL) {
             char *typeName = NULL;
             ErrorCode error = readZarrString(archive, count, &typeName);
-            printf("%s\n",typeName);
             
             if (error != SUCCESS) {
+                fprintf(stderr, "Error reading Zarr data type: %s\n", getErrorMessage(error));
                 return error;
             }
+            printf("%s\n",typeName);
             
             // Set the pixel type based on string content
             if (typeName) {
