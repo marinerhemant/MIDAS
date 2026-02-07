bd849f7dcd61bdc3ef0f88f845348eb49bd30dc1
Hemant Sharma <hemant@sharmas.biz>
2026-02-07 17:16:06 -0600

    Debug

--- FF_HEDM/src/FitPosOrStrainsOMP.c
+++ FF_HEDM/src/FitPosOrStrainsOMP.c
@@ -2095,20 +2095,21 @@ int main(int argc, char *argv[]) {
       size_t spotPosAllSpots;
       for (i = 0; i < nSpotsBest; i++) {
         spotPosAllSpots = (int)spotIDS[i] - 1;
-        // check if spotPosAllSpots + 1 > nSpots, then print and error and kill
-        // the process
+        // check if spotPosAllSpots + 1 > nSpots, then print and error and
+        // continue to next spot
         if (spotPosAllSpots >= nSpots || spotPosAllSpots < 0) {
           printf("Data mismatch! Original: %d, Looked for "
-                 "spotNr %d, nSpots %d, skipping this spot.\n",
-                 (int)spotIDS[i], (int)(spotPosAllSpots + 1), (int)nSpots);
+                 "spotNr %d, nSpots %d, skipping this spot, GrID: %d.\n",
+                 (int)spotIDS[i], (int)(spotPosAllSpots + 1), (int)nSpots,
+                 (int)thisRowNr);
           fflush(stdout);
           continue;
         }
         if (spotPosAllSpots + 1 != (size_t)AllSpots[spotPosAllSpots * 14 + 4]) {
           printf("Data mismatch! Original: %d, Looked for %d, found %d, "
-                 "skipping this spot.\n",
+                 "skipping this spot, GrID: %d.\n",
                  (int)spotIDS[i], (int)(spotPosAllSpots + 1),
-                 (int)AllSpots[spotPosAllSpots * 14 + 4]);
+                 (int)AllSpots[spotPosAllSpots * 14 + 4], (int)thisRowNr);
           fflush(stdout);
           continue;
         }
