4d22e065db5dbf043bf5b62650f3f569d79fbe4a
Hemant Sharma <hemant@sharmas.biz>
2026-02-24 10:28:25 -0600

    localConfig: remove deprecated Parsl Config parameters

--- FF_HEDM/workflows/localConfig.py
+++ FF_HEDM/workflows/localConfig.py
@@ -2,10 +2,6 @@ from parsl.config import Config
 from parsl.executors import ThreadPoolExecutor
 
 localConfig = Config(
-    app_cache=True, 
-    checkpoint_files=None, 
-    checkpoint_mode=None, 
-    checkpoint_period=None, 
     executors=(ThreadPoolExecutor(
         label='threads', 
         max_threads=1, 
@@ -13,7 +9,6 @@ localConfig = Config(
         thread_name_prefix='', 
         working_dir=None
     ),), 
-    garbage_collect=True, 
     initialize_logging=True, 
     internal_tasks_max_threads=1, 
     max_idletime=120.0, 
