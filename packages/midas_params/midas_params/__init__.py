c0d4bee3d903d73fb580be4f40287938dcc91987
Hemant Sharma <hemant@sharmas.biz>
2026-04-23 14:35:37 -0500

    midas-params: bump version to 0.3.0

--- packages/midas_params/midas_params/__init__.py
+++ packages/midas_params/midas_params/__init__.py
@@ -8,7 +8,7 @@ Public surface (stable):
   - VALIDATORS, resolve                                            (validator lookup)
 """
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from .schema import (
     CrossFieldRule,
