64c83b3c1c83780e46f2d1d2d8097816769d449d
Hemant Sharma <hemant@sharmas.biz>
2026-05-20 20:49:16 -0500

    midas-fit-grain: bump version to 0.4.1

--- packages/midas_fit_grain/midas_fit_grain/__init__.py
+++ packages/midas_fit_grain/midas_fit_grain/__init__.py
@@ -5,7 +5,7 @@ Drop-in replacement for the C executables ``FitPosOrStrainsOMP`` /
 under module-private submodules.
 """
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 from .config import FitConfig
 from .device import resolve_device, resolve_dtype
