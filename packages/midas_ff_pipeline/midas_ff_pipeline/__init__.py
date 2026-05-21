ec68aa8ac7d3f54ecd6d9bbd19cbe12649b3bbbc
Hemant Sharma <hemant@sharmas.biz>
2026-05-20 20:56:28 -0500

    midas-ff-pipeline: bump version to 0.3.1

--- packages/midas_ff_pipeline/midas_ff_pipeline/__init__.py
+++ packages/midas_ff_pipeline/midas_ff_pipeline/__init__.py
@@ -12,7 +12,7 @@ Public API:
 
 from __future__ import annotations
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from .config import LayerSelection, MachineConfig, PipelineConfig
 from .detector import DetectorConfig
