9c1c4a0a50d397d6579d56d2f0e34ec4875361df
Hemant Sharma <hemant@sharmas.biz>
2026-05-20 19:57:22 -0500

    midas-process-grains: bump version to 0.2.0

--- packages/midas_process_grains/midas_process_grains/__init__.py
+++ packages/midas_process_grains/midas_process_grains/__init__.py
@@ -19,7 +19,7 @@ Three operating modes (`mode=` kwarg):
 
 from __future__ import annotations
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .params import ProcessGrainsParams, read_paramstest_pg
 
