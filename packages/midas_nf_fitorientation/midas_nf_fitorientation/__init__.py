6fa5c4e792f55021014de599150cc8ffeb3354ef
Hemant Sharma <hemant@sharmas.biz>
2026-08-02 20:21:10 -0500

    midas-nf-fitorientation: bump version to 0.6.0

--- packages/midas_nf_fitorientation/midas_nf_fitorientation/__init__.py
+++ packages/midas_nf_fitorientation/midas_nf_fitorientation/__init__.py
@@ -23,7 +23,7 @@ The Python API exposes the same three drivers::
     )
 """
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 from .params import FitParams, parse_paramfile
 from .fit_orientation import fit_orientation_run
